b96316955b5a30aa4fd4eef5fc0daf70e5e4654f
Jamie Scaife <JamieOnUbuntu@users.noreply.github.com>
2017-04-07 00:07:00 +0100

    Update and rename index.html to index.php

--- other/speedrun-times/index.php
+++ other/speedrun-times/index.php
@@ -15,17 +15,7 @@
 
 <body>
 
-<nav>
-    <div class="navbar">
-        <h5>
-            <a href="/"><img src="/images/js-circle-48.png" width="3.5%" height="3.5%"></a>
-            <a href="/">Home</a>
-            <a href="/blog/">Blog</a>
-            <a href="/projects/">Projects</a>
-            <a href="/other/">Other</a>
-        </h5>
-    </div>
-</nav>
+<?php include "navbar.php" ?>
 
 <div class="body">
     <h1>Speedrun Times</h1>
@@ -84,10 +74,7 @@
     06. Sunday 9th March 2015: <b>00:25:50</b></pre>
 </div>
 
-<footer>
-    <hr>
-    <h5>Copyright &copy Jamie Scaife 2017 | <a href="/contact/">Contact Information</a></h5>
-</footer>
+<?php include "footer.php" ?>
 
 </body>
 
