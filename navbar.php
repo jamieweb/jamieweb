36dbdadedcad47f1358ac766e0b8fbd934d323e8
Jamie Scaife <jamie@jamieweb.net>
2017-04-07 00:31:07 +0100

    Removed breadcrumb
    Working on a new implementation of this

--- navbar.php
+++ navbar.php
@@ -2,7 +2,7 @@
     <div class="navbar">
         <h5>
             <a href="/"><img src="/images/js-circle-48.png" width="3.5%" height="3.5%"></a>
-            <a href="/" class="current">Home</a>
+            <a href="/">Home</a>
             <a href="/blog/">Blog</a>
             <a href="/projects/">Projects</a>
             <a href="/other/">Other</a>
