d8156a1f4e0d028aa0ebb34789dd3c1afc22d019
Jamie Scaife <JamieOnUbuntu@users.noreply.github.com>
2017-04-06 23:47:23 +0100

    Update and rename index.html to index.php

--- blog/new-site-design/index.php
+++ blog/new-site-design/index.php
@@ -15,17 +15,7 @@
 
 <body>
 
-<nav>
-    <div class="navbar">
-        <h5>
-            <a href="/"><img src="/images/js-circle-48.png" width="3.5%" height="3.5%"></a>
-            <a href="/">Home</a>
-            <a href="/blog/">Blog</a>
-            <a href="/projects/">Projects</a>
-            <a href="/other/">Other</a>
-        </h5>
-    </div>
-</nav>
+<?php include "navbar.php" ?>
 
 <div class="body">
     <h1>New Site Design</h1>
@@ -36,10 +26,7 @@
     <p>If you notice any mistakes, errors or 404 links, please let me know. Thanks!</p>
 </div>
 
-<footer>
-    <hr>
-    <h5>Copyright &copy Jamie Scaife 2017 | <a href="/contact/">Contact Information</a></h5>
-</footer>
+<?php include "footer.php" ?>
 
 </body>
 
