4f144921b9b3b72158ad76c7db055e3e6a0fb164
Jamie Scaife <JamieOnUbuntu@users.noreply.github.com>
2017-04-08 22:43:29 +0100

    New and improved navbar
    Now using list instead of what was essentially a sentence
    Links now have hover color: #4c4c4c, which is the grey colour from the homepage identicon
    Will now be easier to implement dropdown menus in the future

--- navbar.php
+++ navbar.php
@@ -1,11 +1,11 @@
 <nav>
     <div class="navbar">
-        <h5>
-            <a href="/"><img src="/images/js-circle-48.png" width="3.5%" height="3.5%"></a>
-            <a href="/">Home</a>
-            <a href="/blog/">Blog</a>
-            <a href="/projects/">Projects</a>
-            <a href="/other/">Other</a>
-        </h5>
+        <h5><ul>
+            <li class="navlogo"><a href="/"><img src="/images/js-circle-48.png" width="35px" height="35px"></a></li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/blog/">Blog</a></li>
+            <li><a href="/projects/">Projects</a></li>
+            <li><a href="/other/">Other</a></li>
+        </ul></h5>
     </div>
 </nav>
