913b0240c8400ff08ef45432d82403722869aeab
Jamie Scaife <JamieOnUbuntu@users.noreply.github.com>
2017-04-09 02:22:59 +0100

    Added dropdown menus with links to pages for each category

--- navbar.php
+++ navbar.php
@@ -1,11 +1,44 @@
 <nav>
     <div class="navbar">
-        <h5><ul>
+        <ul>
             <li class="navlogo"><a href="/"><img src="/images/js-circle-48.png" width="35px" height="35px"></a></li>
-            <li><a href="/">Home</a></li>
-            <li><a href="/blog/">Blog</a></li>
-            <li><a href="/projects/">Projects</a></li>
-            <li><a href="/other/">Other</a></li>
-        </ul></h5>
+            <li><h5><a href="/">Home</a></li></h5>
+            <li class="dropdown">
+                <h5><a href="/blog/">Blog</a></h5>
+                <div class="dropdown-content">
+                    <h4><a href="/blog/chrome-site-whitelist-extension/">Chrome Site Whitelist Extension</a>
+                    <a href="/blog/subgraph-os-overview/">Subgraph OS Overview</a>
+                    <a href="/blog/tor-hidden-service/">TOR Hidden Service</a>
+                    <a href="/blog/block-exploitable-content/">Blocking Exploitable Content</a>
+                    <a href="/blog/proof-of-timestamp/">Proof Of Timestamp</a>
+                    <a href="/blog/identicon-gravity-animation/">Identicon Gravity Animation</a>
+                    <a href="/blog/new-site-design/">New Site Design + Layout</a>
+                    <a href="/blog/plainsight-enciphering-demo/">Plainsight Enciphering Demo</a>
+                    <a href="/blog/iphone-strange-clock/">iPhone System Clock</a>
+                    <a href="/blog/ubuntu-phone-review/">Ubuntu Phone Review</a>
+                    <a href="/blog/nghl-windows-guide/">NGHL Windows Guide</a>
+                    <a href="/blog/nghl-linux-guide/">NGHL Linux Guide</a></h4>
+                </div>
+            </li>
+            <li class="dropdown">
+                <h5><a href="/projects/">Projects</a></h5>
+                <div class="dropdown-content">
+                    <h4><a href="/projects/computing-stats/">Computing Stats</a>
+                    <a href="/projects/block-exploitable-content/">Exploitable Web Content</a>
+                    <a href="/projects/chrome-site-whitelist-extension/">Chrome Site Whitelist Extension</a></h4>
+                </div>
+            </li>
+            <li class="dropdown">
+            <h5><a href="/other/">Other</a></h5>
+                <div class="dropdown-content">
+                    <h4><a href="/downloads/">Downloads</a>
+                    <a href="/checksums/">Checksums</a>
+                    <a href="/other/askin-art-gallery/">ASKIN Art Gallery</a>
+                    <a href="/other/speedrun-times/">Speedrun Times</a>
+                    <a href="/other/game-tracker/">Game Tracker</a>
+                    <a href="/other/chrome-extension-ids/">Chrome Extension IDs</a></h4>
+                </div>
+            </li>
+        </ul>
     </div>
 </nav>
