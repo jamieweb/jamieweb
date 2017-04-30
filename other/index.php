a0c6e8810f75e48e96d173f11ff177d9375ea389
Jamie Scaife <jamie@jamieweb.net>
2017-04-30 01:38:29 +0100

    New icons for each link
    Icons from FontAwesome (fontawesome.io)
    ASKIN Art Icon by Christopher Askin, thank you!

--- other/index.php
+++ other/index.php
@@ -21,26 +21,75 @@
     <h1>Other</h1>
     <hr>
 
-    <h3 class="no-mar-bottom"><a href="/downloads/">Downloads</a></h3>
-    <p class="two-mar-top"><b>Links to download various useful files.</b></p>
+    <div class="iconparent">
+        <div class="iconimg">
+            <a href="/downloads/"><img src="/images/downloads.png"></a>
+        </div>
+        <div class="iconlist">
+            <h3 class="no-mar-bottom"><a href="/downloads/">Downloads</a></h3>
+            <p class="two-mar-top"><b>Links to download various useful files.</b></p>
+        </div>
+    </div>
 
-    <h3 class="no-mar-bottom"><a href="/other/checksums/">Checksums</a></h3>
-    <p class="two-mar-top"><b>A list of checksums for various PC games/mods/tools.</b></p>
+    <div class="iconparent">
+        <div class="iconimg">
+            <a href="/other/checksums/"><img src="/images/checksums.png"></a>
+        </div>
+        <div class="iconlist">
+            <h3 class="no-mar-bottom"><a href="/other/checksums/">Checksums</a></h3>
+            <p class="two-mar-top"><b>A list of checksums for various PC games/mods/tools.</b></p>
+        </div>
+    </div>
 
-    <h3 class="no-mar-bottom"><a href="/other/askin-art-gallery/">ASKIN Art Gallery</a></h3>
-    <p class="two-mar-top"><b>Original works by Christopher Askin.</b></p>
+    <div class="iconparent">
+        <div class="iconimg">
+            <a href="/other/askin-art-gallery/"><img src="/images/askin-art.png"></a>
+        </div>
+        <div class="iconlist">
+            <h3 class="no-mar-bottom"><a href="/other/askin-art-gallery/">ASKIN Art Gallery</a></h3>
+            <p class="two-mar-top"><b>Original works by Christopher Askin.</b></p>
+        </div>
+    </div>
 
-    <h3 class="no-mar-bottom"><a href="/other/speedrun-times/">Speedrun Times</a></h3>
-    <p class="two-mar-top"><b>A list of my speedrun times from 2014/2015.</b></p>
+    <div class="iconparent">
+        <div class="iconimg">
+            <a href="/other/speedrun-times/"><img src="/images/speedrun-times.png"></a>
+        </div>
+        <div class="iconlist">
+            <h3 class="no-mar-bottom"><a href="/other/speedrun-times/">Speedrun Times</a></h3>
+            <p class="two-mar-top"><b>A list of my speedrun times from 2014/2015.</b></p>
+        </div>
+    </div>
 
-    <h3 class="no-mar-bottom"><a href="/other/game-tracker/">Non-Steam Game Time Tracker</a></h3>
-    <p class="two-mar-top"><b>Game time tracker for non-Steam games.</b></p>
+    <div class="iconparent">
+        <div class="iconimg">
+            <a href="/other/game-tracker/"><img src="/images/game-tracker.png"></a>
+        </div>
+        <div class="iconlist">
+            <h3 class="no-mar-bottom"><a href="/other/game-tracker/">Non-Steam Game Time Tracker</a></h3>
+            <p class="two-mar-top"><b>Game time tracker for non-Steam games.</b></p>
+        </div>
+    </div>
 
-    <h3 class="no-mar-bottom"><a href="/other/chrome-extension-ids/">Chrome Extension IDs</a></h3>
-    <p class="two-mar-top"><b>IDs and links of the legitimate versions of popular Chrome extensions.</b></p>
+    <div class="iconparent">
+        <div class="iconimg">
+            <a href="/other/chrome-extension-ids/"><img src="/images/chrome-extensions.png"></a>
+        </div>
+        <div class="iconlist">
+            <h3 class="no-mar-bottom"><a href="/other/chrome-extension-ids/">Chrome Extension IDs</a></h3>
+            <p class="two-mar-top"><b>IDs and links of the legitimate versions of popular Chrome extensions.</b></p>
+        </div>
+    </div>
 
-    <h3 class="no-mar-bottom"><a href="/pastes/">Pastes</a></h3>
-    <p class="two-mar-top"><b>Plain text snippets.</b></p>
+    <div class="iconparent">
+        <div class="iconimg">
+            <a href="/pastes/"><img src="/images/pastes.png"></a>
+        </div>
+        <div class="iconlist">
+            <h3 class="no-mar-bottom"><a href="/pastes/">Pastes</a></h3>
+            <p class="two-mar-top"><b>Plain text snippets.</b></p>
+        </div>
+    </div>
 </div>
 
 <?php include "footer.php" ?>
