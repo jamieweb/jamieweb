d63b61a1a45b72d1f5ec4e1ec04c472f296345cf
Jamie Scaife <JamieOnUbuntu@users.noreply.github.com>
2017-05-06 23:55:10 +0100

    IRC information page
    Currently a basic page containing only essential info, working on a much fancier one

---
+++ irc/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!--Copyright Jamie Scaife-->
+<!--Legal Information at https://www.jamieweb.net/contact-->
+
+<head>
+    <title>JamieWeb IRC</title>
+    <meta name="description" content="JamieWeb IRC - irc.jamieweb.net:7000">
+    <meta name="keywords" content="Jamie, Scaife, jamie scaife, jamiescaife, jamieonubuntu, jamie90437, jamie90437x, jamieweb, jamieweb.net">
+    <meta name="author" content="Jamie Scaife">
+    <link href="/jamie.css" rel="stylesheet">
+    <link href="https://www.jamieweb.net/irc/" rel="canonical">
+</head>
+
+<body>
+
+<?php include "navbar.php" ?>
+
+<div class="body">
+    <h1>JamieWeb IRC</h1>
+    <hr>
+    <p>Connect to the JamieWeb IRC server using the details below:</p>
+    <pre class="irc">Host: <b>irc.jamieweb.net</b>
+Port: <b>7000</b>
+SSL Connections Only</pre>
+    <p>Or connect in your browser using the <a href="https://kiwiirc.com/client?settings=d040e0e7958af28d5f6ee4380304e7d1">KiwiIRC WebIRC client</a>.</p>
+    <p>If you're using Irssi, use the following command:</p>
+    <pre>/connect -ssl irc.jamieweb.net 7000 stuff &lt;nickname&gt;</pre>
+    <p>Example:</p>
+    <pre>/connect -ssl irc.jamieweb.net 7000 stuff Peter</pre>
+    <p><b>IMPORTANT NOTE: Do not trust anybody on the IRC claiming to be me (Jamie). I will never speak officially through this IRC.</b></p>
+</div>
+
+<?php include "footer.php" ?>
+
+</body>
+
+</html>
