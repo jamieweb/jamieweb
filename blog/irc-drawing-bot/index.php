e4e770de6383092ad3729a371793fbd4ef363de5
Jamie Scaife <jamie@jamieweb.net>
2017-05-09 00:59:58 +0100

    Placeholder blog post to avoid broken links
    I am still working on the blog post, will release asap

---
+++ blog/irc-drawing-bot/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!--Copyright Jamie Scaife-->
+<!--Legal Information at https://www.jamieweb.net/contact-->
+
+<head>
+    <title>IRC Drawing Bot</title>
+    <meta name="description" content="IRC Drawing Bot">
+    <meta name="keywords" content="Jamie, Scaife, jamie scaife, jamiescaife, jamieonubuntu, jamie90437, jamie90437x, jamieweb, jamieweb.net">
+    <meta name="author" content="Jamie Scaife">
+    <link href="/jamie.css" rel="stylesheet">
+    <link href="https://www.jamieweb.net/blog/irc-drawing-bot/" rel="canonical">
+</head>
+
+<body>
+
+<?php include "navbar.php" ?>
+
+<div class="body">
+    <h1>IRC Drawing Bot</h1>
+    <hr>
+    <p><b>Tuesday 9th May 2017</b></p>
+    <p>Work in progress, coming soon!</p>
+</div>
+
+<?php include "footer.php" ?>
+
+</body>
+
+</html>
