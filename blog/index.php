5456fc2e0516ac9f6a39e3ebba696cd6aed362d6
Jamie Scaife <jamie@jamieweb.net>
2017-07-15 16:09:11 +0100

    Added link to new blog post - hosts-file-integrity

--- blog/index.php
+++ blog/index.php
@@ -30,6 +30,10 @@
             <div class="blog-brace5"></div>
         </div>
         <div class="blog-list">
+            <h3 class="no-mar-bottom"><a href="/blog/hosts-file-integrity/">Hosts File Site Blocking</a></h3>
+            <p class="two-no-mar"><b>Blocking sites using the hosts file and an integrity verification script.</b></p>
+            <p class="two-mar-top">Saturday 15th July 2017</p>
+
             <h3 class="no-mar-bottom"><a href="/blog/inspircd-linux-guide/">How to Install and Configure 'InspIRCd' on Linux</a></h3>
             <p class="two-no-mar"><b>In-depth guide for installing and configuring InspIRCd on Linux.</b></p>
             <p class="two-mar-top">Tuesday 16th May 2017</p>
