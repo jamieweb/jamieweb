9c04bf399f90c7d7be393e1eb3dcda30c443ceaa
Jamie Scaife <JamieOnUbuntu@users.noreply.github.com>
2017-07-29 23:09:03 +0100

    New info/references section
    For random bits of useful/technical information

---
+++ info/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!--Copyright Jamie Scaife-->
+<!--Legal Information at https://www.jamieweb.net/contact-->
+
+<head>
+    <title>Info/Refs</title>
+    <meta name="description" content="Information and References">
+    <meta name="keywords" content="Jamie, Scaife, jamie scaife, jamiescaife, jamieonubuntu, jamie90437, jamie90437x, jamieweb, jamieweb.net">
+    <meta name="author" content="Jamie Scaife">
+    <link href="/jamie.css" rel="stylesheet">
+    <link href="https://www.jamieweb.net/info/" rel="canonical">
+</head>
+
+<body>
+
+<?php include "navbar.php" ?>
+
+<div class="body">
+    <h1>Information and References</h1>
+    <hr>
+
+    <div class="iconparent">
+        <div class="iconimg">
+            <a href="/other/adblock-plus-config/"><img src="/images/adblock-plus-config.png"></a>
+        </div>
+        <div class="iconlist">
+            <h3 class="no-mar-bottom"><a href="/other/adblock-plus-config/">Adblock Plus Configuration</a></h3>
+            <p class="two-mar-top"><b>Extra filter lists and configuration for Adblock Plus.</b></p>
+        </div>
+    </div>
+
+    <div class="iconparent">
+        <div class="iconimg">
+            <a href="/other/chrome-extension-ids/"><img src="/images/chrome-extensions.png"></a>
+        </div>
+        <div class="iconlist">
+            <h3 class="no-mar-bottom"><a href="/other/chrome-extension-ids/">Chrome Extension IDs</a></h3>
+            <p class="two-mar-top"><b>IDs and links of the legitimate versions of popular Chrome extensions.</b></p>
+        </div>
+    </div>
+</div>
+
+<?php include "footer.php" ?>
+
+</body>
+
+</html>
