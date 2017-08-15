435f742f30f32211dd3b4153098bdc4b1b4cfddd
Jamie Scaife <jamie@jamieweb.net>
2017-08-15 18:44:12 +0100

    Fixed Tor capitalisation

--- blog/tor-hidden-service/index.php
+++ blog/tor-hidden-service/index.php
@@ -5,8 +5,8 @@
 <!--Legal Information at https://www.jamieweb.net/contact-->
 
 <head>
-    <title>TOR Hidden Service</title>
-    <meta name="description" content="Setting up a TOR Hidden Service for JamieWeb">
+    <title>Tor Hidden Service</title>
+    <meta name="description" content="Setting up a Tor Hidden Service for JamieWeb">
     <meta name="keywords" content="Jamie, Scaife, jamie scaife, jamiescaife, jamieonubuntu, jamie90437, jamie90437x, jamieweb, jamieweb.net">
     <meta name="author" content="Jamie Scaife">
     <link href="/jamie.css" rel="stylesheet">
@@ -18,11 +18,11 @@
 <?php include "navbar.php" ?>
 
 <div class="body">
-    <h1>TOR Hidden Service</h1>
+    <h1>Tor Hidden Service</h1>
     <hr>
     <p><b>Sunday 12th February 2017</b></p>
-    <p>Last night I launched a TOR Hidden Service for my site, available at <a href="http://jamiewebgbelqfno.onion" target="_blank">jamiewebgbelqfno.onion</a>.</p>
-    <p>It serves exactly the same content as the main surface web site, nothing has been added or removed. My own anonymity is definitely not the purpose of having a TOR hidden service, it's just for demonstrative/research purposes.</p>
+    <p>Last night I launched a Tor Hidden Service for my site, available at <a href="http://jamiewebgbelqfno.onion" target="_blank">jamiewebgbelqfno.onion</a>.</p>
+    <p>It serves exactly the same content as the main surface web site, nothing has been added or removed. My own anonymity is definitely not the purpose of having a Tor hidden service, it's just for demonstrative/research purposes.</p>
     <center><img width="1000px" src="/images/tor-browser-jamieweb.png"></center>
     <p>I generated the vanity onion address using <a href="https://github.com/katmagic/Shallot" target="_blank">Shallot</a> on my Raspberry Pi cluster. I ran the program on all nodes 24/7 for 35 days, and achieved an average hashrate of 1.25 MH/s. I was searching for URLs that matched the following regex pattern:</p>
     <pre>    ^jamie[234567]\|^jamieweb\|^jamiesca\|^jamieonubuntu</pre>
@@ -42,7 +42,7 @@
         awk 'BEGIN {print sprintf("%.0f",('$attempts' / '$runtime') / 1000)}' > hashrate.txt
     done</pre>
     <p>Every time a matching address is found, it will be output to the file for today's date. The hashrate will be outputted to hashrate.txt in KH/s. Shallot then starts again and keeps searching for matching addresses. I used cron to start the script automatically at boot. Use grep to view a list of the addresses that you have found without printing all of the private keys.</p>
-    <p>The original vanity onion address that I have been displaying on my homepage for a while now (jamie6boo7fo44ky.onion) unfortunately did not work. When putting the private key into the TOR Hidden Service config, it produced a completely different address. It seems that a few users of Shallot have experienced similar issues with the keys not matching the addresses. I tried a dozen or so other keys that I generated, including those directly before and after jamie6boo, and they all worked fine.</p>
+    <p>The original vanity onion address that I have been displaying on my homepage for a while now (jamie6boo7fo44ky.onion) unfortunately did not work. When putting the private key into the Tor Hidden Service config, it produced a completely different address. It seems that a few users of Shallot have experienced similar issues with the keys not matching the addresses. I tried a dozen or so other keys that I generated, including those directly before and after jamie6boo, and they all worked fine.</p>
     <p>I am certain that I did not accidentally modify or otherwise change the key for jamie6boo, it was handled in exactly the same way as every other key. I also compared the key to numerous backups, and it had not been changed at all since the original generation. It seems I was just unlucky. :(</p>
     <p>This is a shame, since the name jamie6boo7fo44ky is very easy to remember and also sounds quite goofy. The new address that I chose (jamiewebgbelqfno.onion) is one of one two ^jamieweb addresses that I generated, but unfortunately doesn't have quite the same ring to it as jamie6boo.</p>
     <center><img src="/images/tor-browser-url.png"></center>
