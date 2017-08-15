ff0f254ea285756b43781b8443aee29bb3cffc62
Jamie Scaife <jamie@jamieweb.net>
2017-08-15 18:36:48 +0100

    Added bunnymodpro download, links now open in new tab

--- downloads/index.php
+++ downloads/index.php
@@ -23,6 +23,18 @@
     <p>These files are not created or owned by me. All credit goes to the original authors. External download links are not hyperlinked.</p>
     <h2>Hosted files:</h2>
     <pre>########################################################################
+<b>Bunnymod Pro</b>
+
+Name: bunnymodpro.zip
+Size: 798.9 KB (818,023 bytes)
+MD5: fcdd8b201f3cd481a8daf32ea9c4fca2
+SHA1: 64f01783613317ad6bab5b2871a321032ccd5347
+SHA256: cbb38bf58140bbfe0f62291025032e295f7aacd077e092293cc5c08b4046b917
+VirusTotal: <a href="https://www.virustotal.com/en/file/cbb38bf58140bbfe0f62291025032e295f7aacd077e092293cc5c08b4046b917/analysis/" target="_blank">0/59 Detection Ratio</a>
+Link: <a href="https://www.jamieweb.net/downloads/bunnymodpro.zip" target="_blank">https://www.jamieweb.net/downloads/bunnymodpro.zip</a>
+########################################################################
+
+########################################################################
 <b>Bunnyrace Beta 2</b>
 
 Name: bunnyrace_beta2.bsp
@@ -30,8 +42,8 @@ Size: 2.7 MB (2,803,500 bytes)
 MD5: 2cff96007a35332cef81be81e0756bf9
 SHA1: d16f54b1b8554b2853355ba900c21597caeaf1a1
 SHA256: 1b5cb500b41b5383fe6865347c4fe7cc3cc3bb3b2438dc10d5fde1c68f0c21ba
-VirusTotal: <a href="https://www.virustotal.com/en/file/1b5cb500b41b5383fe6865347c4fe7cc3cc3bb3b2438dc10d5fde1c68f0c21ba/analysis/">0/55 Detection Ratio</a>
-Link: <a href="https://www.jamieweb.net/downloads/bunnyrace_beta2.bsp">https://www.jamieweb.net/downloads/bunnyrace_beta2.bsp</a>
+VirusTotal: <a href="https://www.virustotal.com/en/file/1b5cb500b41b5383fe6865347c4fe7cc3cc3bb3b2438dc10d5fde1c68f0c21ba/analysis/" target="_blank">0/55 Detection Ratio</a>
+Link: <a href="https://www.jamieweb.net/downloads/bunnyrace_beta2.bsp" target="_blank">https://www.jamieweb.net/downloads/bunnyrace_beta2.bsp</a>
 ########################################################################
 
 ########################################################################
@@ -42,8 +54,8 @@ Size: 2.2 MB (2,297,864 bytes)
 MD5: 939d0b2236e257ae2a602badf8268640
 SHA1: 7f7748894fd4f65ed932cb3abf9ea00608c2ae5c
 SHA256: f0ac72b26f6ca157b54c14dd2801c243856d055c589a4882063d3597616091a2
-VirusTotal: <a href="https://www.virustotal.com/en/file/f0ac72b26f6ca157b54c14dd2801c243856d055c589a4882063d3597616091a2/analysis/">0/56 Detection Ratio</a>
-Link: <a href="https://www.jamieweb.net/downloads/ag_longjump2.bsp">https://www.jamieweb.net/downloads/ag_longjump2.bsp</a>
+VirusTotal: <a href="https://www.virustotal.com/en/file/f0ac72b26f6ca157b54c14dd2801c243856d055c589a4882063d3597616091a2/analysis/" target="_blank">0/56 Detection Ratio</a>
+Link: <a href="https://www.jamieweb.net/downloads/ag_longjump2.bsp" target="_blank">https://www.jamieweb.net/downloads/ag_longjump2.bsp</a>
 ########################################################################</pre>
     <h2>External download links:</h2>
     <pre>########################################################################
@@ -59,18 +71,6 @@ Link: http://ngageclan.ucoz.ru/load/nghl/12-1-0-41
 ########################################################################
 
 ########################################################################
-<b>Bunnymod Pro</b>
-
-Name: hlspbunny.7z
-Size: 523.7 KB (536,259 bytes)
-MD5: c51fdbd86f19463437721042793b8261
-SHA1: 9f5ef5dcfb997b3ece551c67fb0f0e3ea99491a7
-SHA256: 9377cc8242322ce954488e637ab44613d7ffe13ae29e7e8bd7d283f78bd33ef2
-VirusTotal: <a href="https://www.virustotal.com/en/file/9377cc8242322ce954488e637ab44613d7ffe13ae29e7e8bd7d283f78bd33ef2/analysis/">0/54 Detection Ratio</a>
-Link: https://dl.dropboxusercontent.com/u/29178100/hlspbunny/hlspbunny.7z
-########################################################################
-
-########################################################################
 <b>AdrenalineGamer Tricks</b>
 
 Name: agtricks.rar
@@ -78,7 +78,7 @@ Size: 1.3 MB (1,362,329 bytes)
 MD5: c5115555faa62eabe48c3ffeb743d984
 SHA1: 648e68b0d83fb1012e87cc3cd7e122a144a5990e
 SHA256: a6d2e31f844b71575169f2df82b979cd0726bf143f85467cfffe6309482a7085
-VirusTotal: <a href="https://www.virustotal.com/en/file/a6d2e31f844b71575169f2df82b979cd0726bf143f85467cfffe6309482a7085/analysis/">0/55 Detection Ratio</a>
+VirusTotal: <a href="https://www.virustotal.com/en/file/a6d2e31f844b71575169f2df82b979cd0726bf143f85467cfffe6309482a7085/analysis/" target="_blank">0/55 Detection Ratio</a>
 Link: https://dl.dropboxusercontent.com/u/9297051/agtricks.rar
 ########################################################################
 
@@ -90,7 +90,7 @@ Size: 2.1 MB (2,252,124 bytes)
 MD5: dff62c4444247a77f435f63476515528
 SHA1: c83c5106aa4a67909d41accdf25545907120acac
 SHA256: 20df9be175665dc27f0603cb6bd6ab2a60c4e597267ba8353fe6419ea32f9736
-VirusTotal: <a href="https://www.virustotal.com/en/file/20df9be175665dc27f0603cb6bd6ab2a60c4e597267ba8353fe6419ea32f9736/analysis/">0/53 Detection Ratio</a>
+VirusTotal: <a href="https://www.virustotal.com/en/file/20df9be175665dc27f0603cb6bd6ab2a60c4e597267ba8353fe6419ea32f9736/analysis/" target="_blank">0/53 Detection Ratio</a>
 Link: http://www.dusty-clan.net/download.php?view.595
 ########################################################################
 
@@ -101,7 +101,7 @@ Size: 83.4 KB (85370 bytes)
 MD5: 22656b212e465243b393684d912bb0c6
 SHA1: c2705b286b676feddab3e62dce4e08d6f81d6b19
 SHA256: 9e1695a469af7686aae19cdab0b1506078fcabbde29079adf1becf285a2d35d6
-VirusTotal: <a href="https://www.virustotal.com/en/file/9e1695a469af7686aae19cdab0b1506078fcabbde29079adf1becf285a2d35d6/analysis/1487084179/">0/54 Detection Ratio</a>
+VirusTotal: <a href="https://www.virustotal.com/en/file/9e1695a469af7686aae19cdab0b1506078fcabbde29079adf1becf285a2d35d6/analysis/" target="_blank">0/54 Detection Ratio</a>
 Link: http://public.sannybuilder.com/GTA4/xliveless-0.999b7.rar
 ########################################################################</pre>
 </div>
