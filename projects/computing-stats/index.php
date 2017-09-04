954cf9ccdfbb172577c9b92e2afad29507c08c2b
Jamie Scaife <jamie@jamieweb.net>
2017-09-04 19:43:42 +0100

    Bitnodes link now opens in new tab

--- projects/computing-stats/index.php
+++ projects/computing-stats/index.php
@@ -29,52 +29,52 @@
             <td bgcolor="limegreen" width="20%"><center><h2>Node 4</h2></center></td>
         </tr>
         <tr>
-            <td width="20%"><p class="info">CPU: <b>99%</b></p>
-            <p class="info">Memory: <b>906 MB</b></p>
-            <p class="info">Disk: <b>4734 MB</b></p>
-            <p class="info">Temp: <b>39.5'C</b></td>
+            <td width="20%"><p class="info">CPU: <b>96.3%</b></p>
+            <p class="info">Memory: <b>914 MB</b></p>
+            <p class="info">Disk: <b>4576 MB</b></p>
+            <p class="info">Temp: <b>43.3'C</b></td>
 
-            <td width="20%"><p class="info">CPU: <b>99.8%</b></p>
-            <p class="info">Memory: <b>323 MB</b></p>
-            <p class="info">Disk: <b>4662 MB</b></p>
-            <p class="info">Temp: <b>37.9'C</b></td>
-
-            <td width="20%"><p class="info">CPU: <b>99.7%</b></p>
-            <p class="info">Memory: <b>321 MB</b></p>
-            <p class="info">Disk: <b>4596 MB</b></p>
-            <p class="info">Temp: <b>40.1'C</b></td>
-
-            <td width="20%"><p class="info">CPU: <b>99.8%</b></p>
-            <p class="info">Memory: <b>321 MB</b></p>
-            <p class="info">Disk: <b>4597 MB</b></p>
-            <p class="info">Temp: <b>41.7'C</b></td>
+            <td width="20%"><p class="info">CPU: <b>97.2%</b></p>
+            <p class="info">Memory: <b>353 MB</b></p>
+            <p class="info">Disk: <b>4016 MB</b></p>
+            <p class="info">Temp: <b>44.9'C</b></td>
 
             <td width="20%"><p class="info">CPU: <b>99.7%</b></p>
-            <p class="info">Memory: <b>320 MB</b></p>
-            <p class="info">Disk: <b>4659 MB</b></p>
-            <p class="info">Temp: <b>41.2'C</b></td>
+            <p class="info">Memory: <b>352 MB</b></p>
+            <p class="info">Disk: <b>4015 MB</b></p>
+            <p class="info">Temp: <b>48.7'C</b></td>
+
+            <td width="20%"><p class="info">CPU: <b>99.7%</b></p>
+            <p class="info">Memory: <b>354 MB</b></p>
+            <p class="info">Disk: <b>4015 MB</b></p>
+            <p class="info">Temp: <b>49.2'C</b></td>
+
+            <td width="20%"><p class="info">CPU: <b>99.7%</b></p>
+            <p class="info">Memory: <b>349 MB</b></p>
+            <p class="info">Disk: <b>4015 MB</b></p>
+            <p class="info">Temp: <b>49.2'C</b></td>
         </tr>
     </table><br>
 
     <table width="100%">
         <tr>
             <td width="50%"><h1 class="info">Rosetta@Home Stats</h1>
-            <p class="info">Total Earned Credits: <b>77,609.88</b></p>
-            <p class="info">Recent Average Credit: <b>358.27</b></p>
-            <p class="info">Total Running Time: <b>203 days</b></p></td>
+            <p class="info">Total Earned Credits: <b>113,816.08</b></p>
+            <p class="info">Recent Average Credit: <b>335.05</b></p>
+            <p class="info">Total Running Time: <b>301 days</b></p></td>
 
             <td width="50%"><h1 class="info">Einstein@Home Stats</h1>
-            <p class="info">Total Earned Credits: <b>174,408.53</b></p>
-            <p class="info">Recent Average Credit: <b>453.35</b></p>
-            <p class="info">Total Running Time: <b>603 days</b></p></td>
+            <p class="info">Total Earned Credits: <b>218,908.53</b></p>
+            <p class="info">Recent Average Credit: <b>480.82</b></p>
+            <p class="info">Total Running Time: <b>701 days</b></p></td>
         </tr>
         <tr>
             <td width="50%" class="stats-padding-top"><h1 class="info">Bitcoin Node Stats</h1>
-            <p class="info">Bitnodes Link: <b><a href="https://bitnodes.21.co/nodes/89.34.99.41-8333/">https://bitnodes.21.co/nodes/89.34.99.41-8333/</a></b></p>
-            <p class="info">Total Running Time: <b>206 days</b></p></td>
+            <p class="info">Bitnodes Link: <b><a href="https://bitnodes.21.co/nodes/89.34.99.41-8333/" target="_blank">https://bitnodes.21.co/nodes/89.34.99.41-8333/</a></b></p>
+            <p class="info">Total Running Time: <b>304 days</b></p></td>
         </tr>
     </table><br>
-    <p class="info two-mar-left">Stats Update Every 10 Minutes. Last Updated: <b>11:10:05pm GMT+1</b></p>
+    <p class="info two-mar-left">Stats Update Every 10 Minutes. Last Updated: <b> 7:42:15pm GMT+1</b></p>
 </div>
 
 <?php include "footer.php" ?>
