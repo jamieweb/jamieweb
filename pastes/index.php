a6df29a99af80e4a5aa0bc544dc69d7d4d13f925
Jamie Scaife <JamieOnUbuntu@users.noreply.github.com>
2017-09-20 00:37:06 +0100

    Fixed canonical link, Pastebin profile link now opens in new tab

--- pastes/index.php
+++ pastes/index.php
@@ -10,7 +10,7 @@
     <meta name="keywords" content="Jamie, Scaife, jamie scaife, jamiescaife, jamieonubuntu, jamie90437, jamie90437x, jamieweb, jamieweb.net">
     <meta name="author" content="Jamie Scaife">
     <link href="/jamie.css" rel="stylesheet">
-    <link href="https://www.jamieweb.net/contact/" rel="canonical">
+    <link href="https://www.jamieweb.net/pastes/" rel="canonical">
 </head>
 
 <body>
@@ -21,7 +21,7 @@
     <h1>Pastes</h1>
     <pre>Adblock Plus Default Filter Lists - <a href="/pastes/adblock-filters.txt">adblock-filters.txt</a>
 New Gauge Half-Life Checksums - <a href="/pastes/nghl-sums.txt">nghl-sums.txt</a></pre>
-    <a href="https://pastebin.com/u/JamieOnUbuntu">https://pastebin.com/u/JamieOnUbuntu</a>
+    <a href="https://pastebin.com/u/JamieOnUbuntu" target="_blank">https://pastebin.com/u/JamieOnUbuntu</a>
 </div>
 
 <?php include "footer.php" ?>
