1943100e265750af3dd576c7349f639835c963f5
Jamie Scaife <JamieOnUbuntu@users.noreply.github.com>
2017-10-14 15:29:10 +0100

    Updated paths

--- notifications/scripts/rate-limit.sh
+++ notifications/scripts/rate-limit.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 if [[ $(echo $1 | egrep "^(add|take)$") ]]; then
-    count=$(cat storage/global-rate-limit.txt)
+    count=$(cat scripts/storage/global-rate-limit.txt)
     sleep 0.1
     if ! [[ $(echo $count | grep [^0-9]) ]]; then
         if [ "$1" == "take" ]; then
-            sed -i '1d' storage/ip-rate-limit.txt
+            iplist=$(sed '1d' scripts/storage/ip-rate-limit.txt)
+            sleep 0.1
+            echo "$iplist" > scripts/storage/ip-rate-limit.txt
             if [ "$count" == "0" ]; then
                 exit
             elif (( "$count" > "0" )); then
-                echo $(($count - 1)) > storage/global-rate-limit.txt
+                echo $(($count - 1)) > scripts/storage/global-rate-limit.txt
             else
-                echo "1440" > storage/global-rate-limit.txt
+                echo "1440" > scripts/storage/global-rate-limit.txt
                 exit
             fi
         elif [ "$1" == "add" ]; then
             if ! (( "$count" >= "0" )); then
-                echo "1440" > storage/global-rate-limit.txt
+                echo "1440" > scripts/storage/global-rate-limit.txt
                 exit
             else
-                echo $(($count + 1)) > storage/global-rate-limit.txt
+                echo $(($count + 1)) > scripts/storage/global-rate-limit.txt
             fi
         else
-            echo "1440" > storage/global-rate-limit.txt
+            echo "1440" > scripts/storage/global-rate-limit.txt
             exit
         fi
     else
-        echo "1440" > storage/global-rate-limit.txt
+        echo "1440" > scripts/storage/global-rate-limit.txt
         exit
     fi
 else
-    echo "1440" > storage/global-rate-limit.txt
+    echo "1440" > scripts/storage/global-rate-limit.txt
     sleep 0.1
     exit
 fi
