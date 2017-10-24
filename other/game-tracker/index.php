5612e830e41edfd92ae2a09e935020a84b2b86ed
Jamie Scaife <jamie@jamieweb.net>
2017-10-24 23:52:36 +0100

    Removed deprecated tags

--- other/game-tracker/index.php
+++ other/game-tracker/index.php
@@ -24,18 +24,18 @@
     <p>Games are timed using /usr/bin/time and converted into the format below using a Python script before been uploaded to the server.</p>
     <hr>
     <div class="hours">
-        <center>
+        <div class="centertext">
             <h2>New Gauge Half-Life</h2>
             <img src="/other/game-tracker/nghl.png">
-        </center>
-        <p class="no-mar-bottom">165.00 hours <span>...since Sat 18th April 2015</span></p>
-        <p class="two-mar-top">8.3 hours <span>...in the past two weeks.</span></p>
+        </div>
+        <p class="no-mar-bottom">283.53 hours <span>...since Sat 18th April 2015</span></p>
+        <p class="two-mar-top">10.5 hours <span>...in the past two weeks.</span></p>
     </div>
     <div class="hours">
-        <center>
+        <div class="centertext">
             <h2>Grand Theft Auto IV</h2>
             <img src="/other/game-tracker/gta4.png">
-        </center>
+        </div>
         <p class="no-mar-bottom">86.32 hours <span>...since Fri 19th June 2015</span></p>
         <p class="two-mar-top">0 hours <span>...in the past two weeks.</span></p> 
     </div>
