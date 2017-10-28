738f0157675deb59260e2c33ffa3b4432aa06502
Jamie Scaife <JamieOnUbuntu@users.noreply.github.com>
2017-10-29 00:20:01 +0100

    Updated timezone since I am no longer BST - need to automate this!

--- projects/computing-stats/index.php
+++ projects/computing-stats/index.php
@@ -29,52 +29,52 @@
             <td bgcolor="limegreen" width="20%"><center><h2>Node 4</h2></center></td>
         </tr>
         <tr>
-            <td width="20%"><p class="info">CPU: <b>96.8%</b></p>
-            <p class="info">Memory: <b>912 MB</b></p>
-            <p class="info">Disk: <b>4534 MB</b></p>
+            <td width="20%"><p class="info">CPU: <b>99.2%</b></p>
+            <p class="info">Memory: <b>580 MB</b></p>
+            <p class="info">Disk: <b>4583 MB</b></p>
+            <p class="info">Temp: <b>37.4'C</b></td>
+
+            <td width="20%"><p class="info">CPU: <b>99.7%</b></p>
+            <p class="info">Memory: <b>352 MB</b></p>
+            <p class="info">Disk: <b>4026 MB</b></p>
+            <p class="info">Temp: <b>39.0'C</b></td>
+
+            <td width="20%"><p class="info">CPU: <b>99.6%</b></p>
+            <p class="info">Memory: <b>352 MB</b></p>
+            <p class="info">Disk: <b>4027 MB</b></p>
             <p class="info">Temp: <b>41.2'C</b></td>
 
-            <td width="20%"><p class="info">CPU: <b>99.8%</b></p>
-            <p class="info">Memory: <b>347 MB</b></p>
-            <p class="info">Disk: <b>3953 MB</b></p>
-            <p class="info">Temp: <b>42.8'C</b></td>
-
             <td width="20%"><p class="info">CPU: <b>99.7%</b></p>
-            <p class="info">Memory: <b>343 MB</b></p>
-            <p class="info">Disk: <b>3952 MB</b></p>
-            <p class="info">Temp: <b>47.1'C</b></td>
+            <p class="info">Memory: <b>408 MB</b></p>
+            <p class="info">Disk: <b>4026 MB</b></p>
+            <p class="info">Temp: <b>42.2'C</b></td>
 
-            <td width="20%"><p class="info">CPU: <b>99.8%</b></p>
-            <p class="info">Memory: <b>343 MB</b></p>
-            <p class="info">Disk: <b>3952 MB</b></p>
-            <p class="info">Temp: <b>47.6'C</b></td>
-
-            <td width="20%"><p class="info">CPU: <b>99.7%</b></p>
-            <p class="info">Memory: <b>345 MB</b></p>
-            <p class="info">Disk: <b>3954 MB</b></p>
-            <p class="info">Temp: <b>47.1'C</b></td>
+            <td width="20%"><p class="info">CPU: <b>98.9%</b></p>
+            <p class="info">Memory: <b>352 MB</b></p>
+            <p class="info">Disk: <b>4027 MB</b></p>
+            <p class="info">Temp: <b>42.2'C</b></td>
         </tr>
     </table><br>
 
     <table width="100%">
         <tr>
             <td width="50%"><h1 class="info">Rosetta@Home Stats</h1>
-            <p class="info">Total Earned Credits: <b>115,381.67</b></p>
-            <p class="info">Recent Average Credit: <b>334.50</b></p>
-            <p class="info">Total Running Time: <b>306 days</b></p></td>
+            <p class="info">Total Earned Credits: <b>129,268.55</b></p>
+            <p class="info">Recent Average Credit: <b>187.38</b></p>
+            <p class="info">Total Running Time: <b>355 days</b></p></td>
 
             <td width="50%"><h1 class="info">Einstein@Home Stats</h1>
-            <p class="info">Total Earned Credits: <b>221,096.03</b></p>
-            <p class="info">Recent Average Credit: <b>437.80</b></p>
-            <p class="info">Total Running Time: <b>706 days</b></p></td>
+            <p class="info">Total Earned Credits: <b>242,408.53</b></p>
+            <p class="info">Recent Average Credit: <b>473.03</b></p>
+            <p class="info">Total Running Time: <b>756 days</b></p></td>
         </tr>
         <tr>
             <td width="50%" class="stats-padding-top"><h1 class="info">Bitcoin Node Stats</h1>
             <p class="info clearboth">Bitnodes Link: <b><a href="https://bitnodes.21.co/nodes/89.34.99.41-8333/" target="_blank">https://bitnodes.21.co/nodes/89.34.99.41-8333/</a></b></p>
-            <p class="info">Total Running Time: <b>309 days</b></p></td>
+            <p class="info">Total Running Time: <b>358 days</b></p></td>
         </tr>
     </table><br>
-    <p class="info two-mar-left">Stats Update Every 10 Minutes. Last Updated: <b> 3:40:05pm GMT+1</b></p>
+    <p class="info two-mar-left">Stats Update Every 10 Minutes. Last Updated: <b>12:10:08am GMT</b></p>
 </div>
 
 <?php include "footer.php" ?>
