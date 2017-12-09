762b3c1b914d30af287711a5037cf0861aba16c2
Jamie Scaife <jamie@jamieweb.net>
2017-12-09 23:05:02 +0000

    Replaced deprecated center tags

--- projects/chrome-site-whitelist-extension/index.php
+++ projects/chrome-site-whitelist-extension/index.php
@@ -22,11 +22,11 @@
     <hr>
     <div class="centertext"><p>-<b><u>Currently early in development and is not yet fully working or tested thoroughly.</u></b>-</p></div>
     <p>A Google Chrome extension that highlights sites on Google/Reddit that have been manually whitelisted by the user.</p>
-    <center><img src="/projects/chrome-site-whitelist-extension/extension-manager.png"></center>
+    <div class="centertext"><img src="/projects/chrome-site-whitelist-extension/extension-manager.png"></div>
     <p>Designed to be an anti-typosquatting tool and to highlight known trusted sites.</p>
-    <center><img src="/projects/chrome-site-whitelist-extension/example-result-highlighting.png"></center>
+    <div class="centertext"><img src="/projects/chrome-site-whitelist-extension/example-result-highlighting.png"></div>
     <p>The extension icon also changes to green when you are currently browsing a whitelisted site.</p>
-    <center><img src="/projects/chrome-site-whitelist-extension/extension-icon-green.png"></center>
+    <div class="centertext"><img src="/projects/chrome-site-whitelist-extension/extension-icon-green.png"></div>
     <p>All code and further details are available on the GitHub respository: <a href="https://github.com/JamieOnUbuntu/results-whitelist/" target="_blank">https://github.com/JamieOnUbuntu/results-whitelist/</a></p>
     <h2>Downloading and Installing:</h2>
     <p>I am considering releasing the extension on the Google Chrome Web Store once it is in a more finished state and has been tested thoroughly.</p>
@@ -41,15 +41,15 @@
             </li>
         </div>
         <div class="ext-r-1">
-            <center><img src="/projects/chrome-site-whitelist-extension/github-download.png"></center><br>
+            <div class="centertext"><img src="/projects/chrome-site-whitelist-extension/github-download.png"></div><br>
         </div>
     </ul>
     <ul><li class="clearboth"><p><b>Open the Chrome extensions manager (Settings -> More Tools -> Extensions, or go to the URL chrome://extensions.</b></p></li></ul>
-    <center><img src="/projects/chrome-site-whitelist-extension/extension-manager-menu.png"></center>
+    <div class="centertext"><img src="/projects/chrome-site-whitelist-extension/extension-manager-menu.png"></div>
     <ul><li><p><b>Enable developer mode by checking the box.</b></p></li></ul>
-    <center><img src="/projects/chrome-site-whitelist-extension/developer-mode.png"></center>
+    <div class="centertext"><img src="/projects/chrome-site-whitelist-extension/developer-mode.png"></div>
     <ul><li><p><b>Click "Load Unpacked Extension" and select the extracted folder containing the extension files.</b></p></li></ul>
-    <center><img width="1000px" src="/projects/chrome-site-whitelist-extension/extension-files.png"></center>
+    <div class="centertext"><img width="1000px" src="/projects/chrome-site-whitelist-extension/extension-files.png"></div>
     <h2>Configuration:</h2>
     <div class="ext-l-2">
         <p class="no-mar-top">You can access the options page by clicking "Options" from the extensions manager, or by right clicking the extension icon at the top right of Chrome.</p>
@@ -60,7 +60,7 @@ www.reddit.com</pre>
         <p>Regular expressions (regex) are not supported.</p>
     </div>
     <div class="ext-r-2">
-        <center><img src="/projects/chrome-site-whitelist-extension/extension-options.png"></center><br>
+        <div class="centertext"><img src="/projects/chrome-site-whitelist-extension/extension-options.png"></div><br>
     </div>
     <p class="clearboth">Click save. Now when you perform a Google search or browse Reddit, links matching those that you defined in the configuration will be highlighed in green. The extension icon at the top right will also change to green when you are currently browsing a whitelisted site.</p>
 </div>
