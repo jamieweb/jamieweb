15acf60831c19c84e678f07b62b70395a11eeb6b
Jamie Scaife <jamie@jamieweb.net>
2017-12-09 23:31:56 +0000

    Now using raw rather than live version, removed content

--- other/game-tracker/index.php
+++ other/game-tracker/index.php
@@ -28,16 +28,8 @@
             <h2>New Gauge Half-Life</h2>
             <img src="/other/game-tracker/nghl.png">
         </div>
-        <p class="no-mar-bottom">283.53 hours <span>...since Sat 18th April 2015</span></p>
-        <p class="two-mar-top">10.5 hours <span>...in the past two weeks.</span></p>
-    </div>
-    <div class="hours">
-        <div class="centertext">
-            <h2>Grand Theft Auto IV</h2>
-            <img src="/other/game-tracker/gta4.png">
-        </div>
-        <p class="no-mar-bottom">86.32 hours <span>...since Fri 19th June 2015</span></p>
-        <p class="two-mar-top">0 hours <span>...in the past two weeks.</span></p> 
+        <p class="no-mar-bottom">var-nghlhours hours <span>...since Sat 18th April 2015</span></p>
+        <p class="two-mar-top">var-nghl2weeks hours <span>...in the past two weeks.</span></p>
     </div>
 </div>
 
