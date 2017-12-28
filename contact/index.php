52b7570213cb50bf29344dac7ee593630e4478c6
Jamie Scaife <JamieOnUbuntu@users.noreply.github.com>
2017-12-28 23:28:03 +0000

    Updated contact page with new emails and cleaned it up generally

--- contact/index.php
+++ contact/index.php
@@ -19,19 +19,27 @@
 
 <div class="body">
     <h1>Contact Information</h1>
-    <p>If you need to contact me for any reason, please do so using the email address listed below or on <a href="https://twitter.com/JamieOnUbuntu" target="_blank" rel="noopener">Twitter</a>.</p>
+    <p>If you need to contact me for any reason, please do so using the email address listed below or via a direct message on <a href="https://twitter.com/JamieOnUbuntu" target="_blank" rel="noopener">Twitter</a>.</p>
     <div class="centertext">
-        <img src="/images/jamie-jamieweb.png" width="50%" height="50%">
+        <img width="388.5px" src="/images/jamie-jamieweb.png">
     </div>
 
     <hr>
     <h1>Security Vulnerability Disclosure</h1>
     <p>I greatly appreciate reports of security vulnerabilities found on my site or other services.</p>
-    <p>I am currently in the process of lauching a HackerOne program for my site, but for the time being please contact me using the email address below or on <a href="https://twitter.com/JamieOnUbuntu" target="_blank" rel="noopener">Twitter</a>.</p>
+    <p>I am currently in the process of lauching a HackerOne program for my site, but for the time being please contact me using the email address below or via a direct message on <a href="https://twitter.com/JamieOnUbuntu" target="_blank" rel="noopener">Twitter</a>.</p>
     <div class="centertext">
-        <img src="/images/security-jamieweb.png" width="50%" height="50%">
+        <img width="428px" src="/images/security-jamieweb.png">
     </div>
     <p>I also appreciate general bug reports, including compatibility issues, spelling/grammatical errors, broken links or incorrect information. Thank you!</p>
+
+    <hr>
+    <h1>Network/Email Abuse</h1>
+    <p>If you are experiencing any form of network abuse or unwanted email coming from my domain or systems, please let me know using the email address below or via a direct message on <a href="https://twitter.com/JamieOnUbuntu" target="_blank" rel="noopener">Twitter</a>.</p>
+    <div class="centertext">
+        <img width="392.5px" src="/images/abuse-jamieweb.png">
+    </div>
+    <p>Any unwanted activity or abuse is most likely spoofed, as my systems do not make any unauthorized connections to any third-party systems or send any automated unsolicited email.</p>
 
     <hr>
     <h1>Legal Information</h1>
@@ -40,11 +48,6 @@
     <p>All content found here that has no attribution is believed to be in the public domain.</p>
     <p>If you are the rightful owner of any of the content on this site, and you do not want it to be displayed or you require suitable credit, then please contact me and I will do whatever is needed either for the content to be removed or to provide credit where it is due.</p>
     <p>Although I do my best to verify its legitimacy, I am not responsible for any of the content/downloads on external sites that are linked here.</p>
-
-    <hr>
-    <h1>Responsible Disclosure</h1>
-    <p>I greatly appreciate reports of security vulnerabilities found on my site or other services.</p>
-    <p>I also appreciate general bug reports, including compatibility issues, spelling/grammatical errors, broken links or incorrect information. Thank you!</p>
 
     <hr>
     <h1>Donations</h1>
