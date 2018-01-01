278cb3ce6a62eda5ebf0cd689540ba093ee1ba04
Jamie Scaife <jamie@jamieweb.net>
2018-01-01 01:48:43 +0000

    Updated year to 2018

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 <footer>
     <hr>
-    <h5>Copyright &copy Jamie Scaife 2017 | <a href="/contact/">Contact Information</a></h5>
+    <h5>Copyright &copy Jamie Scaife 2018 | <a href="/contact/">Contact Information</a></h5>
 </footer>
