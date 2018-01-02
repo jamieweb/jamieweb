ec137e897bd0285cb9402edcf73e75258783e4d5
Jamie Scaife <jamie@jamieweb.net>
2018-01-02 23:59:58 +0000

    Added extra contact links

--- footer.php
+++ footer.php
@@ -1,4 +1,11 @@
 <footer>
     <hr>
-    <h5>Copyright &copy Jamie Scaife 2018 | <a href="/contact/">Contact Information</a></h5>
+    <h5 class="footer-content">Copyright &copy Jamie Scaife 2018 | 
+<a href="https://github.com/JamieOnUbuntu" target="_blank" rel="noopener"><img title="GitHub" width="28px" src="/images/gh-90.png"></a>
+<a href="https://twitter.com/JamieOnUbuntu" target="_blank" rel="noopener"><img title="Twitter" width="28px" src="/images/tw-90.png"></a>
+<a href="https://www.youtube.com/jamie90437x" target="_blank" rel="noopener"><img title="YouTube" width="28px" src="/images/yt-90.png"></a>
+<a href="https://keybase.io/JamieOnUbuntu" target="_blank" rel="noopener"><img title="Keybase" width="28px" src="/images/kb-90.png"></a>
+<a href="/jamie-scaife.asc" target="_blank"><img title="PGP" width="28px" src="/images/pgp-90.png"></a>
+<a href="/rss.xml" target="_blank"><img title="RSS" width="24px" src="/images/rss-90.png"></a>
+ | <a href="/contact/">Contact Information</a></h5>
 </footer>
