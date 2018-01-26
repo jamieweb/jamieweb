bdc552e5155508d7524c062158444441a61fd47e
Jamie Scaife <JamieOnUbuntu@users.noreply.github.com>
2018-01-26 23:14:58 +0000

    Now CC-BY-SA 4.0, updated footer
    Considering switching to CC-BY 4.0 in the future, however trying this first.

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer>
     <hr>
-    <h5 class="footer-content">Copyright &copy Jamie Scaife 2018 | 
+    <h5 class="footer-content two-mar-bottom">Copyright &copy Jamie Scaife 2018 | 
 <a href="https://github.com/JamieOnUbuntu" target="_blank" rel="noopener"><img width="28px" src="/images/gh-90.png"></a>
 <a href="https://twitter.com/JamieOnUbuntu" target="_blank" rel="noopener"><img width="28px" src="/images/tw-90.png"></a>
 <a href="https://www.youtube.com/jamie90437x" target="_blank" rel="noopener"><img width="28px" src="/images/yt-90.png"></a>
@@ -8,4 +8,5 @@
 <a href="/jamie-scaife.asc" target="_blank"><img width="28px" src="/images/pgp-90.png"></a>
 <a href="/rss.xml" target="_blank"><img width="24px" src="/images/rss-90.png"></a>
  | <a href="/contact/">Contact Information</a></h5>
+    <h5 class="two-mar-top">This work is licensed under a <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank">Creative Commons Attribution-ShareAlike 4.0 International License</a>.</h5>
 </footer>
