1b113d8d6eeba895ec23db596250913b6c50f041
Jamie Scaife <JamieOnUbuntu@users.noreply.github.com>
2018-02-09 22:40:05 +0000

    Added rel="noopener" to links, updated description

--- info/chrome-extension-ids/index.php
+++ info/chrome-extension-ids/index.php
@@ -6,7 +6,7 @@
 
 <head>
     <title>Chrome Extension IDs</title>
-    <meta name="description" content="Chrome Extension IDs">
+    <meta name="description" content="IDs and URLs of the legitimate versions of popular Chrome extensions.">
     <meta name="keywords" content="Jamie, Scaife, jamie scaife, jamiescaife, jamieonubuntu, jamie90437, jamie90437x, jamieweb, jamieweb.net">
     <meta name="author" content="Jamie Scaife">
     <link href="/jamie.css" rel="stylesheet">
@@ -23,23 +23,23 @@
     <p><b>Why have I posted these here?</b></p>
     <p>The Google Chrome Web Store is known for having malicious apps and extensions listed among the legitimate ones, so I have listed the proper links to a few of the popular ones. The idea behind this is that this page may appear in the Google search results when somebody is searching an extension ID, and may help to verify whether it is legitimate or not.</p>
     <p><b>Popular Security + Privacy Extensions:</b></p>
-    <pre>AdBlock - <a href="https://chrome.google.com/webstore/detail/adblock/gighmmpiobklfepjocnamgkkbiglidom" target="_blank">gighmmpiobklfepjocnamgkkbiglidom</a>
-AdBlock Plus - <a href="https://chrome.google.com/webstore/detail/adblock-plus/cfhdojbkjhnklbpkdaibdccddilifddb" target="_blank">cfhdojbkjhnklbpkdaibdccddilifddb</a>
-uBlock - <a href="https://chrome.google.com/webstore/detail/ublock/epcnnfbjfcgphgdmggkamkmgojdagdnn" target="_blank">epcnnfbjfcgphgdmggkamkmgojdagdnn</a>
-uBlock Origin - <a href="https://chrome.google.com/webstore/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm" target="_blank">cjpalhdlnbpafiamejdnhcphjbkeiagm</a>
-Ghostery - <a href="https://chrome.google.com/webstore/detail/ghostery/mlomiejdfkolichcflejclcbmpeaniij" target="_blank">mlomiejdfkolichcflejclcbmpeaniij</a>
-Privacy Badger - <a href="https://chrome.google.com/webstore/detail/privacy-badger/pkehgijcmpdhfbdbbnkijodmdjhbjlgp" target="_blank">pkehgijcmpdhfbdbbnkijodmdjhbjlgp</a>
-Disconnect - <a href="https://chrome.google.com/webstore/detail/disconnect/jeoacafpbcihiomhlakheieifhpjdfeo" target="_blank">jeoacafpbcihiomhlakheieifhpjdfeo</a>
-HTTPS Everywhere - <a href="https://chrome.google.com/webstore/detail/https-everywhere/gcbommkclmclpchllfjekcdonpmejbdp" target="_blank">gcbommkclmclpchllfjekcdonpmejbdp</a>
-VTchromizer - <a href="https://chrome.google.com/webstore/detail/vtchromizer/efbjojhplkelaegfbieplglfidafgoka" target="_blank">efbjojhplkelaegfbieplglfidafgoka</a>
-Dr Web Link Checker - <a href="https://chrome.google.com/webstore/detail/drweb-anti-virus-link-che/aleggpabliehgbeagmfhnodcijcmbonb" target="_blank">aleggpabliehgbeagmfhnodcijcmbonb</a></pre>
+    <pre>AdBlock - <a href="https://chrome.google.com/webstore/detail/adblock/gighmmpiobklfepjocnamgkkbiglidom" target="_blank" rel="noopener">gighmmpiobklfepjocnamgkkbiglidom</a>
+AdBlock Plus - <a href="https://chrome.google.com/webstore/detail/adblock-plus/cfhdojbkjhnklbpkdaibdccddilifddb" target="_blank" rel="noopener">cfhdojbkjhnklbpkdaibdccddilifddb</a>
+uBlock - <a href="https://chrome.google.com/webstore/detail/ublock/epcnnfbjfcgphgdmggkamkmgojdagdnn" target="_blank" rel="noopener">epcnnfbjfcgphgdmggkamkmgojdagdnn</a>
+uBlock Origin - <a href="https://chrome.google.com/webstore/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm" target="_blank" rel="noopener">cjpalhdlnbpafiamejdnhcphjbkeiagm</a>
+Ghostery - <a href="https://chrome.google.com/webstore/detail/ghostery/mlomiejdfkolichcflejclcbmpeaniij" target="_blank" rel="noopener">mlomiejdfkolichcflejclcbmpeaniij</a>
+Privacy Badger - <a href="https://chrome.google.com/webstore/detail/privacy-badger/pkehgijcmpdhfbdbbnkijodmdjhbjlgp" target="_blank" rel="noopener">pkehgijcmpdhfbdbbnkijodmdjhbjlgp</a>
+Disconnect - <a href="https://chrome.google.com/webstore/detail/disconnect/jeoacafpbcihiomhlakheieifhpjdfeo" target="_blank" rel="noopener">jeoacafpbcihiomhlakheieifhpjdfeo</a>
+HTTPS Everywhere - <a href="https://chrome.google.com/webstore/detail/https-everywhere/gcbommkclmclpchllfjekcdonpmejbdp" target="_blank" rel="noopener">gcbommkclmclpchllfjekcdonpmejbdp</a>
+VTchromizer - <a href="https://chrome.google.com/webstore/detail/vtchromizer/efbjojhplkelaegfbieplglfidafgoka" target="_blank" rel="noopener">efbjojhplkelaegfbieplglfidafgoka</a>
+Dr Web Link Checker - <a href="https://chrome.google.com/webstore/detail/drweb-anti-virus-link-che/aleggpabliehgbeagmfhnodcijcmbonb" target="_blank" rel="noopener">aleggpabliehgbeagmfhnodcijcmbonb</a></pre>
     <p><b>Official Google Extensions/Apps that are installed by default:</b></p>
-    <pre>Google Mail - <a href="https://chrome.google.com/webstore/detail/gmail/pjkljhegncpnkpknbcohdijeoejaedia" target="_blank">pjkljhegncpnkpknbcohdijeoejaedia</a>
-Google Drive - <a href="https://chrome.google.com/webstore/detail/google-drive/apdfllckaahabafndbhieahigkjlhalf" target="_blank">apdfllckaahabafndbhieahigkjlhalf</a>
-Google Docs - <a href="https://chrome.google.com/webstore/detail/google-docs/aohghmighlieiainnegkcijnfilokake" target="_blank">aohghmighlieiainnegkcijnfilokake</a>
-Google Sheets - <a href="https://chrome.google.com/webstore/detail/google-sheets/felcaaldnbdncclmgdcncolpebgiejap" target="_blank">felcaaldnbdncclmgdcncolpebgiejap</a>
-Google Slides - <a href="https://chrome.google.com/webstore/detail/google-slides/aapocclcgogkmnckokdopfmhonfmgoek" target="_blank">aapocclcgogkmnckokdopfmhonfmgoek</a>
-YouTube - <a href="https://chrome.google.com/webstore/detail/youtube/blpcfgokakmgnkcojhhkbfbldkacnbeo" target="_blank">blpcfgokakmgnkcojhhkbfbldkacnbeo</a></pre>
+    <pre>Google Mail - <a href="https://chrome.google.com/webstore/detail/gmail/pjkljhegncpnkpknbcohdijeoejaedia" target="_blank" rel="noopener">pjkljhegncpnkpknbcohdijeoejaedia</a>
+Google Drive - <a href="https://chrome.google.com/webstore/detail/google-drive/apdfllckaahabafndbhieahigkjlhalf" target="_blank" rel="noopener">apdfllckaahabafndbhieahigkjlhalf</a>
+Google Docs - <a href="https://chrome.google.com/webstore/detail/google-docs/aohghmighlieiainnegkcijnfilokake" target="_blank" rel="noopener">aohghmighlieiainnegkcijnfilokake</a>
+Google Sheets - <a href="https://chrome.google.com/webstore/detail/google-sheets/felcaaldnbdncclmgdcncolpebgiejap" target="_blank" rel="noopener">felcaaldnbdncclmgdcncolpebgiejap</a>
+Google Slides - <a href="https://chrome.google.com/webstore/detail/google-slides/aapocclcgogkmnckokdopfmhonfmgoek" target="_blank" rel="noopener">aapocclcgogkmnckokdopfmhonfmgoek</a>
+YouTube - <a href="https://chrome.google.com/webstore/detail/youtube/blpcfgokakmgnkcojhhkbfbldkacnbeo" target="_blank" rel="noopener">blpcfgokakmgnkcojhhkbfbldkacnbeo</a></pre>
     <p><b>Official Google Extensions/Apps that are now built into Chrome and no longer available on the store:</b></p>
     <pre>Google Wallet - nmmhkkegccagdldgiimedpiccmgmieda
 Chrome PDF Viewer - mhjfbmdgcfjbbpaeojofohoefgiehjai
