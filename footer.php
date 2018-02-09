d03e33273a52ffb5ad18ab0e67d12125cd0ece4c
Jamie Scaife <JamieOnUbuntu@users.noreply.github.com>
2018-02-09 19:47:47 +0000

    Updated images to Font Awesome 5

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
 <footer>
     <hr>
     <h5 class="footer-content two-mar-bottom">Copyright &copy Jamie Scaife 2018 | 
-<a href="https://github.com/JamieOnUbuntu" target="_blank" rel="noopener"><img width="28px" src="/images/gh-90.png"></a>
-<a href="https://twitter.com/JamieOnUbuntu" target="_blank" rel="noopener"><img width="28px" src="/images/tw-90.png"></a>
-<a href="https://www.youtube.com/jamie90437x" target="_blank" rel="noopener"><img width="28px" src="/images/yt-90.png"></a>
-<a href="https://keybase.io/JamieOnUbuntu" target="_blank" rel="noopener"><img width="28px" src="/images/kb-90.png"></a>
-<a href="/jamie-scaife.asc" target="_blank"><img width="28px" src="/images/pgp-90.png"></a>
-<a href="/rss.xml" target="_blank"><img width="24px" src="/images/rss-90.png"></a>
+<a href="https://github.com/JamieOnUbuntu" target="_blank" rel="noopener"><img width="28px" src="/images/fontawesome/github.svg"></a>
+<a href="https://twitter.com/JamieOnUbuntu" target="_blank" rel="noopener"><img width="28px" src="/images/fontawesome/tw.svg"></a>
+<a href="https://www.youtube.com/jamie90437x" target="_blank" rel="noopener"><img width="28px" src="/images/fontawesome/yt.svg"></a>
+<a href="https://keybase.io/JamieOnUbuntu" target="_blank" rel="noopener"><img width="28px" src="/images/fontawesome/id-card.svg"></a>
+<a href="/jamie-scaife.asc" target="_blank"><img width="28px" src="/images/fontawesome/key.svg"></a>
+<a href="/rss.xml" target="_blank"><img width="24px" src="/images/fontawesome/rss.svg"></a>
  | <a href="/contact/">Contact Information</a></h5>
     <h5 class="two-mar-top">This work is licensed under a <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank">Creative Commons Attribution-ShareAlike 4.0 International License</a>.</h5>
 </footer>
