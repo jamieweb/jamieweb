761b78895f2ea30783fd383e1ccdb12b676c2e07
Jamie Scaife <jamie@jamieweb.net>
2018-02-10 22:48:47 +0000

    Firefox extension IDs, not really needed but here for my own use at the very least

---
+++ info/firefox-extension-ids/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!--Copyright Jamie Scaife-->
+<!--Legal Information at https://www.jamieweb.net/contact-->
+
+<head>
+    <title>Firefox Extension URLs</title>
+    <meta name="description" content="URLs of the legitimate versions of popular Firefox extensions.">
+    <meta name="keywords" content="Jamie, Scaife, jamie scaife, jamiescaife, jamieonubuntu, jamie90437, jamie90437x, jamieweb, jamieweb.net">
+    <meta name="author" content="Jamie Scaife">
+    <link href="/jamie.css" rel="stylesheet">
+    <link href="https://www.jamieweb.net/info/firefox-extension-ids/" rel="canonical">
+</head>
+
+<body>
+
+<?php include "navbar.php" ?>
+
+<div class="body">
+    <h1>Firefox Extension IDs</h1>
+    <hr>
+    <p><b>Why have I posted these here?</b></p>
+    <p>This page is designed to complement my <a href="/info/chrome-extension-ids" target="_blank">Chrome Extension IDs</a> list. While the Firefox Add-ons store currently doesn't seem to have the same spam/squatting/impersonation problems as the Chrome Web Store, these links are here for whoever may find them useful.</p>
+    <p><b>Popular Security + Privacy Extensions:</b></p>
+    <pre>AdBlock Plus - <a href="https://addons.mozilla.org/firefox/addon/adblock-plus/" target="_blank">https://addons.mozilla.org/firefox/addon/adblock-plus/</a>
+uBlock Origin- <a href="https://addons.mozilla.org/firefox/addon/ublock-origin/" target="_blank">https://addons.mozilla.org/firefox/addon/ublock-origin/</a>
+Ghostery - <a href="https://addons.mozilla.org/firefox/addon/ghostery/" target="_blank">https://addons.mozilla.org/firefox/addon/ghostery/</a>
+Privacy Badger - <a href="https://addons.mozilla.org/firefox/addon/privacy-badger17/" target="_blank">https://addons.mozilla.org/firefox/addon/privacy-badger17/</a>
+Disconnect - <a href="https://addons.mozilla.org/firefox/addon/disconnect/" target="_blank">https://addons.mozilla.org/firefox/addon/disconnect/</a>
+HTTPS Everywhere - <a href="https://addons.mozilla.org/firefox/addon/https-everywhere/" target="_blank">https://addons.mozilla.org/firefox/addon/https-everywhere/</a></pre>
+</div>
+
+<?php include "footer.php" ?>
+
+</body>
+
+</html>
