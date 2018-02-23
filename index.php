2c2feca42ba293ae30167be1108f41690419e17d
Jamie Scaife <jamie@jamieweb.net>
2018-02-23 23:38:17 +0000

    Updated site description

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <head>
     <title>JamieWeb</title>
-    <meta name="description" content="Website of Jamie Scaife (JamieOnUbuntu).">
+    <meta name="description" content="Website of Jamie Scaife - United Kingdom">
     <meta name="keywords" content="Jamie, Scaife, jamie scaife, jamiescaife, jamieonubuntu, jamie90437, jamie90437x, jamieweb, jamieweb.net">
     <meta name="author" content="Jamie Scaife">
     <link href="/jamie.css" rel="stylesheet">
