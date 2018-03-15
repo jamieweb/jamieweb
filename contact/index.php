753cbab7c74a991243fa02181975854e04315292
Jamie Scaife <jamie@jamieweb.net>
2018-03-15 22:52:04 +0000

    Updated contact page formatting and license info

--- contact/index.php
+++ contact/index.php
@@ -42,26 +42,27 @@
     <p>I accept open DMs on Twitter via <a href="https://twitter.com/JamieOnUbuntu" target="_blank" rel="noopener">@JamieOnUbuntu</a>.</p>
     <hr>
 
-    <h2 class="hackerone"><img class="two-mar-bottom padding-right-10 vertical-align-middle" width="16px" src="/images/hackerone.png">HackerOne</h2>
+    <h2 id="hackerone"><img class="two-mar-bottom padding-right-10 vertical-align-middle" width="16px" src="/images/hackerone.png">HackerOne</h2>
     <p>I currently have a HackerOne security vulnerability disclosure program that is under private/controlled launch. If you would like to be invited, please send me your email address and I'll add you!</p>
-    <p>You can view a copy of the security policy <a href="https://github.com/JamieOnUbuntu/config/blob/master/hackerone/policy.md" target="_blank" rel="noopener">here</a>.</p>
+    <p>You can view a copy of the security policy <a href="https://github.com/JamieOnUbuntu/config/blob/master/hackerone/policy.md" target="_blank" rel="noopener">here</a>, and the scope <a href="https://github.com/JamieOnUbuntu/config/blob/master/hackerone/scope.md" target="_blank" rel="noopener">here</a>.</p>
     <p><b>Please note that my HackerOne program does not provide bounties/rewards as JamieWeb is only a small personal project.</b></p>
 
     <hr>
-    <h1>Network/Email Abuse</h1>
+    <h1 id="abuse">Network/Email Abuse</h1>
     <p>If you are experiencing any form of network abuse or unwanted email coming from my domain or systems, please contact me with details including applicable logs and/or headers.</p>
     <p>Any unwanted activity or abuse is most likely spoofed, as my systems do not make any unauthorized connections to any third-party systems or send any automated unsolicited email.</p>
 
     <hr>
-    <h1>Legal Information</h1>
+    <h1 id="legal">Legal Information</h1>
     <ul><li>Homepage sidebar logo: <b>Jdenticon by Daniel Mester Pirttij&#228;rvi</b> (<a href="https://jdenticon.com" target="_blank">https://jdenticon.com</a>)</li>
-    <li>Icons around site: <b>Font Awesome by Dave Gandy</b> (<a href="https://fontawesome.com" target="_blank" rel="noopener">https://fontawesome.com</a>) - Licenced Under <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">CC BY 4.0</a></li></ul>
+    <li>Icons around site: <b>Font Awesome by Dave Gandy</b> (<a href="https://fontawesome.com" target="_blank" rel="noopener">https://fontawesome.com</a>) - Licensed Under <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">CC BY 4.0</a></li>
+    <li>Site-wide font: <b>Open Sans by Steve Matteson</b> (<a href="https://fonts.google.com/specimen/Open+Sans" target="_blank" rel="noopener">https://fonts.google.com/specimen/Open+Sans</a>) - Licensed Under <a href="https://www.apache.org/licenses/LICENSE-2.0.html" target="_blank">Apache 2.0</a></li></ul>
     <p>All content found here that has no attribution is believed to be in the public domain.</p>
     <p>If you are the rightful owner of any of the content on this site, and you do not want it to be displayed or you require suitable credit, then please contact me and I will do whatever is needed either for the content to be removed or to provide credit where it is due.</p>
     <p>Although I do my best to verify its legitimacy, I am not responsible for any of the content/downloads on external sites that are linked here.</p>
 
     <hr>
-    <h1>Donations</h1>
+    <h1 id="donate">Donations</h1>
     <pre>Bitcoin:  <b>1Jamie</b>PmmYMSHnmtnPvF2eyD1hmdGA1NqF - <a href="https://blockchain.info/address/1JamiePmmYMSHnmtnPvF2eyD1hmdGA1NqF" target="_blank" rel="noopener">blockchain.info</a>
 Litecoin: <b>LXDE</b>bJAzqeUPy2piNq3YtZfNeTrvSo76Mp - <a href="https://live.blockcypher.com/ltc/address/LXDEbJAzqeUPy2piNq3YtZfNeTrvSo76Mp/" target="_blank" rel="noopener">blockcypher.com</a>
 Dogecoin: <b>DDoS</b>rqbJR33uEJkQQJAh8KWf7pSC22RWNh - <a href="https://dogechain.info/address/DDoSrqbJR33uEJkQQJAh8KWf7pSC22RWNh" target="_blank" rel="noopener">dogechain.info</a>
