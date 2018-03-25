1b1a52c1bb73664cbb6f094bdde7db055fdd1c9a
Jamie Scaife <jamie@jamieweb.net>
2018-03-25 23:06:45 +0000

    Fixed missing h4 in navbar, adjusted NBSPs on homepage

--- navbar.php
+++ navbar.php
@@ -14,28 +14,34 @@
             <li class="dropdown">
                 <h5><a href="/projects/">Projects</a></h5>
                 <div class="dropdown-content">
-                    <h4><a href="/projects/computing-stats/">Computing Stats</a>
-                    <a href="/projects/block-exploitable-content/">Exploitable Web Content</a>
-                    <a href="/projects/chrome-site-whitelist-extension/">Chrome Site Whitelist Extension</a>
-                    <a href="/projects/irc-drawing-bot/">IRC Drawing Bot</a></h4>
+                    <h4>
+                        <a href="/projects/computing-stats/">Computing Stats</a>
+                        <a href="/projects/block-exploitable-content/">Exploitable Web Content</a>
+                        <a href="/projects/chrome-site-whitelist-extension/">Chrome Site Whitelist Extension</a>
+                        <a href="/projects/irc-drawing-bot/">IRC Drawing Bot</a>
+                    </h4>
                 </div>
             </li>
             <li class="dropdown">
                 <h5><a href="/info/">Info</a></h5>
                 <div class="dropdown-content">
-                    <h4><a href="/info/adblock-plus-config/">Adblock Plus Configuration</a>
-                    <a href="/info/chrome-extension-ids/">Chrome Extension IDs</a>
-                    <a href="/info/firefox-extension-ids/">Firefox Extension IDs</a>
-                    <a href="/info/hl-client-security/">Half-Life Client Security</a>
+                    <h4>
+                        <a href="/info/adblock-plus-config/">Adblock Plus Configuration</a>
+                        <a href="/info/chrome-extension-ids/">Chrome Extension IDs</a>
+                        <a href="/info/firefox-extension-ids/">Firefox Extension IDs</a>
+                        <a href="/info/hl-client-security/">Half-Life Client Security</a>
+                    </h4>
                 </div>
             </li>
             <li class="dropdown">
             <h5><a href="/other/">Other</a></h5>
                 <div class="dropdown-content">
-                    <h4><a href="/downloads/">Downloads</a>
-                    <a href="/other/speedrun-times/">Speedrun Times</a>
-                    <a href="/other/game-tracker/">Game Tracker</a>
-                    <a href="/pastes/">Pastes</a></h4>
+                    <h4>
+                        <a href="/downloads/">Downloads</a>
+                        <a href="/other/speedrun-times/">Speedrun Times</a>
+                        <a href="/other/game-tracker/">Game Tracker</a>
+                        <a href="/pastes/">Pastes</a>
+                    </h4>
                 </div>
             </li>
         </ul>
