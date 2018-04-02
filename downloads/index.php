f328cc286e900680d24f3c7aea2dc55cbcd429ae
Jamie Scaife <jamie@jamieweb.net>
2018-04-02 23:45:14 +0000

    Updated downloads page

--- downloads/index.php
+++ downloads/index.php
@@ -20,10 +20,10 @@
 <div class="body">
     <h1>Downloads</h1>
     <hr>
-    <p>These files are not created or owned by me. All credit goes to the original authors. External download links are not hyperlinked.</p>
+    <p>These files are not created or owned by me. All credit goes to the original authors as stated next to each file name.</p>
     <h2>Hosted files:</h2>
     <pre>########################################################################
-<b>Bunnymod Pro</b>
+<b>Bunnymod Pro</b> - <a href="https://wiki.sourceruns.org/wiki/Bunnymod_Pro#Credits" target="_blank" rel="noopener">Various Credits</a>
 
 Name: bunnymodpro.zip
 Size: 798.9 KB (818,023 bytes)
@@ -35,7 +35,7 @@ Link: <a href="https://www.jamieweb.net/downloads/bunnymodpro.zip" target="_blan
 ########################################################################
 
 ########################################################################
-<b>Bunnyrace Beta 2</b>
+<b>Bunnyrace Beta 2</b> - Created by author 'Coincidence'
 
 Name: bunnyrace_beta2.bsp
 Size: 2.7 MB (2,803,500 bytes)
@@ -56,54 +56,6 @@ SHA1: 7f7748894fd4f65ed932cb3abf9ea00608c2ae5c
 SHA256: f0ac72b26f6ca157b54c14dd2801c243856d055c589a4882063d3597616091a2
 VirusTotal: <a href="https://www.virustotal.com/en/file/f0ac72b26f6ca157b54c14dd2801c243856d055c589a4882063d3597616091a2/analysis/" target="_blank">0/56 Detection Ratio</a>
 Link: <a href="https://www.jamieweb.net/downloads/ag_longjump2.bsp" target="_blank">https://www.jamieweb.net/downloads/ag_longjump2.bsp</a>
-########################################################################</pre>
-    <h2>External download links:</h2>
-    <pre>########################################################################
-<b>New Gauge Half-Life Installer</b>
-
-Name: NGHL_Full_v1_34.exe
-Size: 196.8 MB (196,753,838 bytes)
-MD5: 1a99a97a7f6309daad549264b1e8734c
-SHA1: 6bb8de2a66be4dc71a57a6fb8e5b592da2119193
-SHA256: 1dd5715962d40dc72242892ad0f15d05833cd7e865bc9b75265e9b450d7b13e3
-VirusTotal: N/A - File Too Large
-Link: http://ngageclan.ucoz.ru/load/nghl/12-1-0-41
-########################################################################
-
-########################################################################
-<b>AdrenalineGamer Tricks</b>
-
-Name: agtricks.rar
-Size: 1.3 MB (1,362,329 bytes)
-MD5: c5115555faa62eabe48c3ffeb743d984
-SHA1: 648e68b0d83fb1012e87cc3cd7e122a144a5990e
-SHA256: a6d2e31f844b71575169f2df82b979cd0726bf143f85467cfffe6309482a7085
-VirusTotal: <a href="https://www.virustotal.com/en/file/a6d2e31f844b71575169f2df82b979cd0726bf143f85467cfffe6309482a7085/analysis/" target="_blank">0/55 Detection Ratio</a>
-Link: https://dl.dropboxusercontent.com/u/9297051/agtricks.rar
-########################################################################
-
-########################################################################
-<b>Deathmatch Classic WAD</b>
-
-Name: dmc.wad
-Size: 2.1 MB (2,252,124 bytes)
-MD5: dff62c4444247a77f435f63476515528
-SHA1: c83c5106aa4a67909d41accdf25545907120acac
-SHA256: 20df9be175665dc27f0603cb6bd6ab2a60c4e597267ba8353fe6419ea32f9736
-VirusTotal: <a href="https://www.virustotal.com/en/file/20df9be175665dc27f0603cb6bd6ab2a60c4e597267ba8353fe6419ea32f9736/analysis/" target="_blank">0/53 Detection Ratio</a>
-Link: http://www.dusty-clan.net/download.php?view.595
-########################################################################
-
-########################################################################
-<b>XLiveLess</b>
-
-Name: xliveless-0.999b7.rar
-Size: 83.4 KB (85370 bytes)
-MD5: 22656b212e465243b393684d912bb0c6
-SHA1: c2705b286b676feddab3e62dce4e08d6f81d6b19
-SHA256: 9e1695a469af7686aae19cdab0b1506078fcabbde29079adf1becf285a2d35d6
-VirusTotal: <a href="https://www.virustotal.com/en/file/9e1695a469af7686aae19cdab0b1506078fcabbde29079adf1becf285a2d35d6/analysis/" target="_blank">0/54 Detection Ratio</a>
-Link: http://public.sannybuilder.com/GTA4/xliveless-0.999b7.rar
 ########################################################################</pre>
 </div>
 
