88ca75b764ca826564a8754cc84c48ee0c7a023b
Jamie Scaife <jamie@jamieweb.net>
2018-04-11 23:15:07 +0000

    Updated pastes list

--- pastes/index.php
+++ pastes/index.php
@@ -19,8 +19,7 @@
 
 <div class="body">
     <h1>Pastes</h1>
-    <pre>Adblock Plus Default Filter Lists - <a href="/pastes/adblock-filters.txt">adblock-filters.txt</a>
-New Gauge Half-Life Checksums - <a href="/pastes/nghl-sums.txt">nghl-sums.txt</a></pre>
+    <pre>Adblock Plus Default Filter Lists - <a href="/pastes/adblock-filters.txt">adblock-filters.txt</a></pre>
     <a href="https://pastebin.com/u/JamieOnUbuntu" target="_blank">https://pastebin.com/u/JamieOnUbuntu</a>
 </div>
 
