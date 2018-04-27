78aca8b002472bc753943135ae4d739e656cdcf4
Jamie Scaife <jamie@jamieweb.net>
2018-04-27 23:25:14 +0000

    Added HackerOne to footer

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
 <footer>
     <hr>
     <h5 class="footer-content two-mar-bottom">Copyright &copy Jamie Scaife 2018 | 
-<a href="https://github.com/JamieOnUbuntu" target="_blank" rel="noopener"><img width="28px" src="/images/fontawesome/github.svg"></a>
-<a href="https://twitter.com/JamieOnUbuntu" target="_blank" rel="noopener"><img width="28px" src="/images/fontawesome/tw.svg"></a>
-<a href="https://www.youtube.com/jamie90437x" target="_blank" rel="noopener"><img width="28px" src="/images/fontawesome/yt.svg"></a>
-<a href="https://keybase.io/JamieOnUbuntu" target="_blank" rel="noopener"><img width="28px" src="/images/fontawesome/id-card.svg"></a>
-<a href="/jamie-scaife.asc" target="_blank"><img width="28px" src="/images/fontawesome/key.svg"></a>
+<a href="https://github.com/JamieOnUbuntu" target="_blank" rel="noopener"><img src="/images/fontawesome/github.svg"></a>
+<a href="https://twitter.com/JamieOnUbuntu" target="_blank" rel="noopener"><img src="/images/fontawesome/tw.svg"></a>
+<a href="https://www.youtube.com/jamie90437x" target="_blank" rel="noopener"><img src="/images/fontawesome/yt.svg"></a>
+<a href="https://keybase.io/JamieOnUbuntu" target="_blank" rel="noopener"><img src="/images/fontawesome/id-card.svg"></a>
+<a href="https://hackerone.com/jamieweb" target="_blank" rel="noopener"><img class="h1" src="/images/hackerone.png"></a>
 <a href="/rss.xml" target="_blank"><img width="24px" src="/images/fontawesome/rss.svg"></a>
  | <a href="/contact/">Contact Information</a></h5>
     <h5 class="two-mar-top">This work is licensed under a <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank" rel="noopener">Creative Commons Attribution-ShareAlike 4.0 International License</a>.</h5>
