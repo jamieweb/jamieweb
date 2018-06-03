aa9de43db7f6eeada309ca0602e112d008515054
Jamie Scaife <jamie@jamieweb.net>
2018-06-03 15:42:12 +0000

    Changed license order, also license now only appears on /blog/*

--- footer.php
+++ footer.php
@@ -1,6 +1,9 @@
 <footer>
     <hr>
-    <h5 class="footer-content two-mar-bottom">Copyright &copy Jamie Scaife 2018 | 
+<?php if(basename(dirname(getcwd())) === "blog") {
+    echo "    <h5 class=\"two-no-mar\">This article is licensed under a <a href=\"https://creativecommons.org/licenses/by-sa/4.0/\" target=\"_blank\" rel=\"noopener\">Creative Commons Attribution-ShareAlike 4.0 International License</a>.</h5>";
+} ?>
+    <h5 class="footer-content">Copyright &copy Jamie Scaife 2018 | 
 <a href="https://github.com/jamieweb" target="_blank" rel="noopener"><img src="/images/fontawesome/github.svg"></a>
 <a href="https://twitter.com/jamieweb" target="_blank" rel="noopener"><img src="/images/fontawesome/tw.svg"></a>
 <a href="https://www.youtube.com/jamie90437x" target="_blank" rel="noopener"><img src="/images/fontawesome/yt.svg"></a>
@@ -8,5 +11,4 @@
 <a href="https://hackerone.com/jamieweb" target="_blank" rel="noopener"><img class="h1" src="/images/hackerone.png"></a>
 <a href="/rss.xml" target="_blank"><img width="24px" src="/images/fontawesome/rss.svg"></a>
  | <a href="/contact/">Contact Information</a></h5>
-    <h5 class="two-mar-top">This work is licensed under a <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank" rel="noopener">Creative Commons Attribution-ShareAlike 4.0 International License</a>.</h5>
 </footer>
