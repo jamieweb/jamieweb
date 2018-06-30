2a2f253882c51302d9773a3faa3a58b8da14b8df
Jamie Scaife <jamie@jamieweb.net>
2018-06-30 23:49:08 +0000

    Moved exploitable web content blocking test, added /tools index

---
+++ tools/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!--Copyright Jamie Scaife-->
+<!--Legal Information at https://www.jamieweb.net/contact-->
+
+<head>
+    <title>Info & Refs</title>
+    <meta name="description" content="Tools">
+    <meta name="keywords" content="Jamie, Scaife, jamie scaife, jamiescaife, jamieonubuntu, jamie90437, jamie90437x, jamieweb, jamieweb.net">
+    <meta name="author" content="Jamie Scaife">
+    <link href="/jamie.css" rel="stylesheet">
+    <link href="https://www.jamieweb.net/tools/" rel="canonical">
+</head>
+
+<body>
+
+<?php include "navbar.php" ?>
+
+<div class="body">
+    <h1>Tools</h1>
+    <hr>
+
+    <div class="iconparent">
+        <div class="iconimg">
+            <a href="/tools/exploitable-web-content-blocking-test/"><img src="/images/fontawesome/bug.svg"></a>
+        </div>
+        <div class="iconlist">
+            <h3 class="no-mar-bottom"><a href="/tools/exploitable-web-content-blocking-test/">Exploitable Web Content Blocking Test</a></h3>
+            <p class="two-mar-top"><b>Test whether exploitable web content is blocked in your browser.</b></p>
+        </div>
+    </div>
+
+    <div class="iconparent">
+        <div class="iconimg">
+            <a href="/tools/ipv6-ipv4-test/"><img src="/images/fontawesome/globe.svg"></a>
+        </div>
+        <div class="iconlist">
+            <h3 class="no-mar-bottom"><a href="/tools/ipv6-ipv4-test/">IPv6 / IPv4 Connection Test</a></h3>
+            <p class="two-mar-top"><b>Test whether you have working IPv6 and IPv4 internet connections.</b></p>
+        </div>
+    </div>
+</div>
+
+<?php include "footer.php" ?>
+
+</body>
+
+</html>
