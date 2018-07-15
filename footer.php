e21de5565cfd304b3ee0ae24a823d6fef7ede39d
Jamie Scaife <jamie@jamieweb.net>
2018-07-15 17:01:04 +0000

    New full-width footer design

--- footer.php
+++ footer.php
@@ -1,14 +1,66 @@
-<footer>
-    <hr>
 <?php if(basename(dirname(getcwd())) === "blog") {
-    echo "    <h5 class=\"two-no-mar\">This article is licensed under a <a href=\"https://creativecommons.org/licenses/by-sa/4.0/\" target=\"_blank\" rel=\"noopener\">Creative Commons Attribution-ShareAlike 4.0 International License</a>.</h5>";
+        echo "    <div class=\"centertext\">
+        <h5 class=\"license\">This article is licensed under a <a href=\"https://creativecommons.org/licenses/by-sa/4.0/\" target=\"_blank\" rel=\"noopener\">Creative Commons Attribution-ShareAlike 4.0 International License</a>.</h5>
+    </div>";
 } ?>
-    <h5 class="footer-content">Copyright &copy Jamie Scaife 2018 | 
-<a href="https://github.com/jamieweb" target="_blank" rel="noopener"><img src="/images/fontawesome/github.svg"></a>
-<a href="https://twitter.com/jamieweb" target="_blank" rel="noopener"><img src="/images/fontawesome/tw.svg"></a>
-<a href="https://www.youtube.com/jamie90437x" target="_blank" rel="noopener"><img src="/images/fontawesome/yt.svg"></a>
-<a href="https://keybase.io/jamieweb" target="_blank" rel="noopener"><img src="/images/fontawesome/id-card.svg"></a>
-<a href="https://hackerone.com/jamieweb" target="_blank" rel="noopener"><img class="h1" src="/images/hackerone.png"></a>
-<a href="/rss.xml" target="_blank"><img width="24px" src="/images/fontawesome/rss.svg"></a>
- | <a href="/contact/">Contact Information</a></h5>
+<footer>
+    <div class="footer">
+        <div class="footer-column">
+            <h2><a href="/"><img class="footer-logo" src="/images/jamieweb.png">JamieWeb</a></h2>
+            <p><b><a href="/blog/">Blog</a> | <a href="/projects/">Projects</a> | <a href="/tools/">Tools</a> | <a href="/info/">Info</a></b></p>
+            <p class="footer-email"><b>Email:</b> <img class="two-mar-left" src="/images/jamie-jamieweb-4d4d4d.png"></p>
+            <p><b><a href="/contact/">Other Contact Info</a> | <a href="/security/">Security</a></b></p>
+            <p>Copyright &copy; Jamie Scaife 2018</p>
+        </div>
+        <div class="footer-column footer-middle">
+            <h2>Links</h2>
+            <a href="https://github.com/jamieweb" target="_blank" rel="noopener">
+                <div class="footer-item">
+                    <img src="/images/fontawesome/github.svg">
+                    <h4>GitHub</h4>
+                </div>
+            </a>
+            <a href="https://twitter.com/jamieweb" target="_blank" rel="noopener">
+                <div class="footer-item">
+                    <img src="/images/fontawesome/tw.svg">
+                    <h4>Twitter</h4>
+                </div>
+            </a>
+            <a href="https://www.youtube.com/jamie90437x" target="_blank" rel="noopener">
+                <div class="footer-item">
+                    <img src="/images/fontawesome/yt.svg">
+                    <h4>YouTube</h4>
+                </div>
+            </a>
+            <a href="https://keybase.io/jamieweb" target="_blank" rel="noopener">
+                <div class="footer-item">
+                    <img src="/images/fontawesome/id-card.svg">
+                    <h4>Keybase</h4>
+                </div>
+            </a>
+            <a href="https://hackerone.com/jamieweb" target="_blank" rel="noopener">
+                <div class="footer-item">
+                    <img class="h1" src="/images/hackerone.png">
+                    <h4>HackerOne</h4>
+                </div>
+            </a>
+            <a href="/rss.xml" target="_blank">
+                <div class="footer-item">
+                    <img src="/images/fontawesome/rss.svg">
+                    <h4>RSS Feed</h4>
+                </div>
+            </a>
+        </div>
+        <div class="footer-column">
+            <h2>Development</h2>
+            <ul class="footer-list">
+                <!--<li><h4><a href="/development/">Overview</a></h4></li>-->
+                <li><h4><a href="https://github.com/jamieweb/jamieweb" target="_blank" rel="noopener">GitHub Repo</a></h4></li>
+                <li><h4><a href="https://gitlab.com/jamieweb/jamieweb" target="_blank" rel="noopener">GitLab Repo</a></h4></li>
+                <li><h4><a href="https://bitbucket.org/jamieweb/jamieweb/src/master" target="_blank" rel="noopener">Bitbucket Repo</a></h4></li>
+                <li><h4><a href="https://git.launchpad.net/jamieweb/" target="_blank" rel="noopener">Launchpad Repo</a></h4></li>
+                <li><h4><a href="https://travis-ci.org/jamieweb/jamieweb" target="_blank" rel="noopener">Travis-CI</a></h4></li>
+            </ul>
+        </div>
+    </div>
 </footer>
