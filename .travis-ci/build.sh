5ce5dd10a77dcca860b5dcee1f6b1adf4689c217
Jamie Scaife <jamie@jamieweb.net>
2018-07-22 20:01:16 +0000

    Added sleep as an attempt at prevening PHP from segfaulting when Travis terminates it

--- .travis-ci/build.sh
+++ .travis-ci/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 node .travis-ci/crawl.js
+sleep 1
 printf "\nCrawled Pages:\n$(cat .travis-ci/crawled.csv)\n\n"
 if [ -s .travis-ci/csp-reports.txt ]; then printf "Content Security Policy Reports:\n$(cat .travis-ci/csp-reports.txt)"; else printf "No Reports Generated! Success!"; fi
