7e181d62b18be6bacfd24c3ca5cbe0f60edcb725
Jamie Scaife <jamie@jamieweb.net>
2018-07-22 18:14:53 +0000

    Added support for csp-report exclusions

--- .travis-ci/build.sh
+++ .travis-ci/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 node .travis-ci/crawl.js
-printf "\nCrawled Pages:\n$(cat .travis-ci/crawled.csv)\n\nContent Security Policy Reports:\n$(cat .travis-ci/csp-reports.txt)"
+printf "\nCrawled Pages:\n$(cat .travis-ci/crawled.csv)\n\n"
+if [ -s .travis-ci/csp-reports.txt ]; then printf "Content Security Policy Reports:\n$(cat .travis-ci/csp-reports.txt)"; else printf "No Reports Generated! Success!"; fi
