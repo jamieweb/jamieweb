afe173eab67ab611595cd486dd275d95b38c549b
Jamie Scaife <jamie@jamieweb.net>
2018-10-08 00:19:00 +0100

    Added boilerplate table styling, started Git SSH server key fingerprints page

---
+++ info/git-hosting-service-ssh-server-key-fingerprints/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!--Copyright Jamie Scaife-->
+<!--Legal Information at https://www.jamieweb.net/contact-->
+
+<head>
+    <title>Git Hosting Service SSH Server Key Fingerprints</title>
+    <meta name="description" content="The SSH server key fingerprints for various online Git hosting services.">
+    <meta name="keywords" content="Jamie, Scaife, jamie scaife, jamiescaife, jamieonubuntu, jamie90437, jamie90437x, jamieweb, jamieweb.net">
+    <meta name="author" content="Jamie Scaife">
+    <link href="/jamie.css" rel="stylesheet">
+    <link href="https://www.jamieweb.net/info/git-hosting-service-ssh-server-key-fingerprints/" rel="canonical">
+</head>
+
+<body>
+
+<?php include "navbar.php" ?>
+
+<div class="body">
+    <h1>Git Hosting Service SSH Server Key Fingerprints</h1>
+    <hr>
+    <p>This page contains the SSH server key fingerprints for various online Git hosting services.</p>
+    <h2>GitHub</h2>
+    <table class="width-1000 padding-6 border-1-4c4c4c border-collapse">
+        <tr class="bg-lightgreen">
+            <th>Algorithm</th>
+            <th>Type</th>
+            <th>Fingerprint</th>
+        </tr>
+        <tr>
+            <td>RSA</td>
+            <td>SHA256</td>
+            <td><code>nThbg6kXUpJWGl7E1IGOCspRomTxdCARLviKw6E5SY8</code></td>
+        </tr>
+        <tr>
+            <td>DSA</td>
+            <td>SHA256</td>
+            <td><code>br9IjFspm1vxR3iA35FWE+4VTyz1hYVLIE2t1/CeyWQ</code></td>
+        </tr>
+        <tr>
+            <td>RSA</td>
+            <td>MD5</td>
+            <td><code>16:27:ac:a5:76:28:2d:36:63:1b:56:4d:eb:df:a6:48</code></td>
+        </tr>
+        <tr>
+            <td>DSA</td>
+            <td>MD5</td>
+            <td><code>ad:1c:08:a4:40:e3:6f:9c:f5:66:26:5d:4b:33:5d:8c</code></td>
+        </tr>
+    </table>
+
+    <h2>GitLab</h2>
+    <h2>Bitbucket</h2>
+    <h2>Launchpad</h2>
+    <p>It is much more CSS heavy, makes use of some of the new HTML5 page layout tags, and will be generally easier to make changes to in the future.</p>
+    <p>If you notice any mistakes, errors or 404 links, please let me know. Thanks!</p>
+</div>
+
+<?php include "footer.php" ?>
+
+</body>
+
+</html>
