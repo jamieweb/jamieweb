d6aa1ca1930b1cee5511e1a03efde0f1c2c917b0
Jamie Scaife <jamie@jamieweb.net>
2018-10-12 23:06:44 +0100

    Added GitLab fingerprints, moved source to bottom

--- info/git-hosting-service-ssh-server-key-fingerprints/index.php
+++ info/git-hosting-service-ssh-server-key-fingerprints/index.php
@@ -23,7 +23,6 @@
     <p>This page contains the SSH server key fingerprints for various online Git hosting services.</p>
     <p>These fingerprints allow you to verify the identity of the SSH server that you are connecting to.</p>
     <h2>GitHub</h2>
-    <p>Source: <a href="https://help.github.com/articles/github-s-ssh-key-fingerprints/" target="_blank" rel="noopener">https://help.github.com/articles/github-s-ssh-key-fingerprints/</a></p>
     <table class="width-1000 padding-6 border-1-4c4c4c border-collapse">
         <tr class="bg-lightgreen">
             <th>Algorithm</th>
@@ -51,8 +50,56 @@
             <td><code>ad:1c:08:a4:40:e3:6f:9c:f5:66:26:5d:4b:33:5d:8c</code></td>
         </tr>
     </table>
+    <p>Source: <a href="https://help.github.com/articles/github-s-ssh-key-fingerprints/" target="_blank" rel="noopener">https://help.github.com/articles/github-s-ssh-key-fingerprints/</a></p>
 
     <h2>GitLab</h2>
+    <table class="width-1000 padding-6 border-1-4c4c4c border-collapse">
+        <tr class="bg-lightgreen">
+            <th>Algorithm</th>
+            <th>Type</th>
+            <th>Fingerprint</th>
+        </tr>
+        <tr>
+            <td>ECDSA</td>
+            <td>SHA256</td>
+            <td><code>HbW3g8zUjNSksFbqTiUWPWg2Bq1x8xdGUrliXFzSnUw</code></td>
+        </tr>
+        <tr>
+            <td>ED25519</td>
+            <td>SHA256</td>
+            <td><code>eUXGGm1YGsMAS7vkcx6JOJdOGHPem5gQp4taiCfCLB8</code></td>
+        </tr>
+        <tr>
+            <td>RSA</td>
+            <td>SHA256</td>
+            <td><code>ROQFvPThGrW4RuWLoL9tq9I9zJ42fK4XywyRtbOz/EQ</code></td>
+        </tr>
+        <tr>
+            <td>DSA</td>
+            <td>SHA256</td>
+            <td><code>p8vZBUOR0XQz6sYiaWSMLmh0t9i8srqYKool/Xfdfqw</code></td>
+        </tr>
+        <tr>
+            <td>ECDSA</td>
+            <td>MD5</td>
+            <td><code>f1:d0:fb:46:73:7a:70:92:5a:ab:5d:ef:43:e2:1c:35</code></td>
+        </tr>
+        <tr>
+            <td>ED25519</td>
+            <td>MD5</td>
+            <td><code>2e:65:6a:c8:cf:bf:b2:8b:9a:bd:6d:9f:11:5c:12:16</code></td>
+        </tr>
+        <tr>
+            <td>RSA</td>
+            <td>MD5</td>
+            <td><code>b6:03:0e:39:97:9e:d0:e7:24:ce:a3:77:3e:01:42:09</code></td>
+        </tr>
+        <tr>
+            <td>DSA</td>
+            <td>MD5</td>
+            <td><code>7a:47:81:3a:ee:89:89:64:33:ca:44:52:3d:30:d4:87</code></td>
+        </tr>
+    </table>
     <h2>Bitbucket</h2>
     <h2>Launchpad</h2>
     <p>It is much more CSS heavy, makes use of some of the new HTML5 page layout tags, and will be generally easier to make changes to in the future.</p>
