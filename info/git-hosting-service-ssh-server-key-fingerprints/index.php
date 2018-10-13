31b364333663ea35ba302f312697da3c213c2974
Jamie Scaife <jamie@jamieweb.net>
2018-10-13 23:12:35 +0100

    Finished Git-as-a-service SSH fingerprints, added bg-lightgrey class, updated RSS with new /info page

--- info/git-hosting-service-ssh-server-key-fingerprints/index.php
+++ info/git-hosting-service-ssh-server-key-fingerprints/index.php
@@ -21,8 +21,9 @@
     <h1>Git Hosting Service SSH Server Key Fingerprints</h1>
     <hr>
     <p>This page contains the SSH server key fingerprints for various online Git hosting services.</p>
-    <p>These fingerprints allow you to verify the identity of the SSH server that you are connecting to.</p>
-    <h2>GitHub</h2>
+    <p>These fingerprints help you to verify the identity of the SSH server that you are connecting to.</p>
+
+    <h2 id="github">GitHub</h2>
     <table class="width-1000 padding-6 border-1-4c4c4c border-collapse">
         <tr class="bg-lightgreen">
             <th>Algorithm</th>
@@ -52,7 +53,7 @@
     </table>
     <p>Source: <a href="https://help.github.com/articles/github-s-ssh-key-fingerprints/" target="_blank" rel="noopener">https://help.github.com/articles/github-s-ssh-key-fingerprints/</a></p>
 
-    <h2>GitLab</h2>
+    <h2 id="gitlab">GitLab</h2>
     <table class="width-1000 padding-6 border-1-4c4c4c border-collapse">
         <tr class="bg-lightgreen">
             <th>Algorithm</th>
@@ -100,10 +101,100 @@
             <td><code>7a:47:81:3a:ee:89:89:64:33:ca:44:52:3d:30:d4:87</code></td>
         </tr>
     </table>
-    <h2>Bitbucket</h2>
-    <h2>Launchpad</h2>
-    <p>It is much more CSS heavy, makes use of some of the new HTML5 page layout tags, and will be generally easier to make changes to in the future.</p>
-    <p>If you notice any mistakes, errors or 404 links, please let me know. Thanks!</p>
+    <p>Source: <a href="https://docs.gitlab.com/ee/user/gitlab_com/#ssh-host-keys-fingerprints" target="_blank" rel="noopener">https://docs.gitlab.com/ee/user/gitlab_com/#ssh-host-keys-fingerprints</a></p>
+
+    <h2 id="bitbucket">Bitbucket</h2>
+    <table class="width-1000 padding-6 border-1-4c4c4c border-collapse">
+        <tr class="bg-lightgreen">
+            <th>Algorithm</th>
+            <th>Type</th>
+            <th>Fingerprint</th>
+        </tr>
+        <tr>
+            <td>RSA</td>
+            <td>SHA256</td>
+            <td><code>zzXQOXSRBEiUtuE8AikJYKwbHaxvSc0ojez9YXaGp1A</code></td>
+        </tr>
+        <tr>
+            <td>DSA</td>
+            <td>SHA256</td>
+            <td><code>RezPkAnH1sowiJM0NQXH90IohWdzHc3fAisEp7L3O3o</code></td>
+        </tr>
+        <tr>
+            <td>RSA</td>
+            <td>MD5</td>
+            <td><code>97:8c:1b:f2:6f:14:6b:5c:3b:ec:aa:46:46:74:7c:40</code></td>
+        </tr>
+        <tr>
+            <td>DSA</td>
+            <td>MD5</td>
+            <td><code>35:ee:d7:b8:ef:d7:79:e2:c6:43:9e:ab:40:6f:50:74</code></td>
+        </tr>
+    </table>
+    <p>Source: <a href="https://confluence.atlassian.com/bitbucket/ssh-keys-935365775.html" target="_blank" rel="noopener">https://confluence.atlassian.com/bitbucket/ssh-keys-935365775.html</a></p>
+
+    <h2 id="launchpad">Launchpad</h2>
+    <p>Bazaar and Ubuntu Package hosting is also provided by Launchpad, so I have included these fingerprints too for completeness.</p>
+    <table class="width-1000 padding-6 border-1-4c4c4c border-collapse">
+        <tr class="bg-lightgreen">
+            <th>Algorithm</th>
+            <th>Type</th>
+            <th>Fingerprint</th>
+        </tr>
+        <tr>
+            <td class="bg-lightgrey" colspan="3">git.launchpad.net:</td>
+        </tr>
+        <tr>
+            <td>RSA</td>
+            <td>SHA256</td>
+            <td><code>UNOzlP66WpDuEo34Wgs8mewypV0UzqHLsIFoqwe8dYo</code></td>
+        </tr>
+        <tr>
+            <td>RSA</td>
+            <td>MD5</td>
+            <td><code>df:b0:16:7e:55:54:96:58:79:85:ba:e2:c3:72:d9:09</code></td>
+        </tr>
+        <tr>
+            <td class="bg-lightgrey" colspan="3">bazaar.launchpad.net:</td>
+        </tr>
+        <tr>
+            <td>RSA</td>
+            <td>SHA256</td>
+            <td><code>dS2DmMhdbMsWaFP4HOF7A/ut73ozMR/gDL2Xxs01/7A</code></td>
+        </tr>
+        <tr>
+            <td>RSA</td>
+            <td>MD5</td>
+            <td><code>9d:38:3a:63:b1:d5:6f:c4:44:67:53:49:2e:ee:fc:89</code></td>
+        </tr>
+        <tr>
+            <td class="bg-lightgrey" colspan="3">ppa.launchpad.net:</td>
+        </tr>
+        <tr>
+            <td>RSA</td>
+            <td>SHA256</td>
+            <td><code>MGq+4hxD7RduVTcfwlwwboZnsgJC6SL/NltM8ye+gNg</code></td>
+        </tr>
+        <tr>
+            <td>RSA</td>
+            <td>MD5</td>
+            <td><code>6b:03:de:98:33:25:23:18:a6:46:b3:47:22:cd:54:f2</code></td>
+        </tr>
+        <tr>
+            <td class="bg-lightgrey" colspan="3">upload.ubuntu.com:</td>
+        </tr>
+        <tr>
+            <td>RSA</td>
+            <td>SHA256</td>
+            <td><code>FN8sNU/MMmyvw/xtY5sAzkLGmkVQt2QpGZcwsHoBzjc</code></td>
+        </tr>
+        <tr>
+            <td>RSA</td>
+            <td>MD5</td>
+            <td><code>79:57:63:97:d3:d3:be:b6:6d:da:81:d0:73:29:80:48</code></td>
+        </tr>
+    </table>
+    <p>Source: <a href="https://help.launchpad.net/SSHFingerprints" target="_blank" rel="noopener">https://help.launchpad.net/SSHFingerprints</a></p>
 </div>
 
 <?php include "footer.php" ?>
