65e32fa9e150bdaab19c5e424237e3ff808e095a
Jamie Scaife <jamie@jamieweb.net>
2018-10-22 14:24:54 +0100

    Updated img srcs

--- blog/radare2-cutter-part-1-key-terminology-and-overview/index.php
+++ blog/radare2-cutter-part-1-key-terminology-and-overview/index.php
@@ -33,12 +33,12 @@
             </ul>
         </div>
         <div class="display-flex flex-align-center flex-justify-center flex-direction-column">
-            <img src="cutter-logo.png" width="250px" title="Cutter Logo" alt="Cutter Logo">
+            <img src="/blog/radare2-cutter-part-1-key-terminology-and-overview/cutter-logo.png" width="250px" title="Cutter Logo" alt="Cutter Logo">
             <p class="no-mar-top centertext"><i>The Cutter logo.</i></p>
         </div>
     </div>
     <p class="no-mar-top">Cutter can be found on GitHub here: <b><a href="https://github.com/radareorg/cutter" target="_blank" rel="noopener">https://github.com/radareorg/cutter</a></b></p>
-    <img class="radius-8" src="cutter-interface-overview.png" width="1000px" title="The Cutter Interface" alt="Screenshot of the Cutter Interface">
+    <img class="radius-8" src="/blog/radare2-cutter-part-1-key-terminology-and-overview/cutter-interface-overview.png" width="1000px" title="The Cutter Interface" alt="Screenshot of the Cutter Interface">
     <p class="two-no-mar centertext"><i>The main Cutter interface.</i></p>
     <p><b>Skip to Section:</b></p>
     <pre><b>Introduction to Reverse Engineering with radare2 Cutter
@@ -69,14 +69,14 @@ Part 1: Key Terminology and Overview</b>
 
     <h2 id="what-are-radare2-and-cutter">What are radare2 and Cutter?</h2>
     <p>Radare2 is an open-source, command-line based reverse engineering framework for Linux, macOS, Windows and many other platforms. It includes a set of tools for reverse engineering and analysing executable files (compiled programs). Radare2 can be used to perform both static and dynamic analysis.</p>
-    <img class="radius-8" src="radare2-interface.png" width="1000px" title="The radare2 Interface Running in the Command-line" alt="A screenshot of the radare2 interface running in the command-line.">
+    <img class="radius-8" src="/blog/radare2-cutter-part-1-key-terminology-and-overview/radare2-interface.png" width="1000px" title="The radare2 Interface Running in the Command-line" alt="A screenshot of the radare2 interface running in the command-line.">
     <p class="two-no-mar centertext"><i>The radare2 command-line interface in disassembly view.</i></p>
     <p>Cutter is the official GUI for radare2, allowing you to make use of all of the features of the command-line version while being able to better organise the information on your screen and make use of additional tools such as the built-in Jupyter notebook.</p>
     <p>Development of Cutter, which was originally named Iaito, started in March 2017. Since then there have been 10 major releases, with the latest version at the time of writing being 1.7.2.</p>
 
     <h2 id="installing-cutter">Installing Cutter</h2>
     <p>Cutter can be acquired in either source or binary form from the official GitHub repository: <a href="https://github.com/radareorg/cutter" target="_blank" rel="noopener">https://github.com/radareorg/cutter</a></p>
-    <img class="radius-8 border-1px-solid" src="radareorg-cutter-github.png" width="1000px" title="The radare2 Cutter project on GitHub" alt="A screenshot of the radare2 Cutter repository on GitHub.">
+    <img class="radius-8 border-1px-solid" src="/blog/radare2-cutter-part-1-key-terminology-and-overview/radareorg-cutter-github.png" width="1000px" title="The radare2 Cutter project on GitHub" alt="A screenshot of the radare2 Cutter repository on GitHub.">
     <p class="two-no-mar centertext"><i>The radare2 Cutter project on GitHub.</i></p>
     <p>Each release is available as an AppImage (for Linux), DMG (for macOS) and a ZIP containing an EXE (for Windows). The source code is also available if you to wish to compile Cutter yourself.</p>
     <p>Unfortunately it's not easy to verify the integrity of the Cutter releases. I have opened an issue about this <a href="https://github.com/radareorg/cutter/issues/666" target="_blank" rel="noopener">here</a>.</p>
@@ -104,26 +104,26 @@ Part 1: Key Terminology and Overview</b>
 
     <h2 id="interface-and-tools">Cutter Interface and Tools</h2>
     <p>Once you have downloaded Cutter (and installed if required), you can run it and choose a file to analyse.</p>
-    <img class="radius-8" src="cutter-open-file.png" width="1000px" title="Choosing a file to open in Cutter" alt="A screenshot of the interface for choosing a file to open in Cutter.">
+    <img class="radius-8" src="/blog/radare2-cutter-part-1-key-terminology-and-overview/cutter-open-file.png" width="1000px" title="Choosing a file to open in Cutter" alt="A screenshot of the interface for choosing a file to open in Cutter.">
     <p class="two-no-mar centertext"><i>Choosing a file to open in Cutter.</i></p>
     <p>If you just want to get Cutter working and analyse a file, a good starting choice could be a basic system tool/program such as <code>pwd</code>, <code>true</code> or <code>whoami</code>.</p>
     <p>After selecting a file, Cutter will allow you to specify the analysis settings. In most cases, these can be left as the default.</p>
-    <img class="radius-8" src="cutter-analysis-settings.png" width="1000px" title="Choosing the Analysis Settings in Cutter" alt="A screenshot of the interface for choosing the analysis settings in Cutter.">
+    <img class="radius-8" src="/blog/radare2-cutter-part-1-key-terminology-and-overview/cutter-analysis-settings.png" width="1000px" title="Choosing the Analysis Settings in Cutter" alt="A screenshot of the interface for choosing the analysis settings in Cutter.">
     <p class="two-no-mar centertext"><i>Choosing the analysis settings in Cutter.</i></p>
     <p>After clicking 'Ok', Cutter will proceed to analyse the file and then the main Cutter interface will appear.</p>
-    <img class="radius-8" src="cutter-default-layout.png" width="1000px" title="The Default Cutter Interface Layout" alt="A screenshot of the default interface layout of Cutter.">
+    <img class="radius-8" src="/blog/radare2-cutter-part-1-key-terminology-and-overview/cutter-default-layout.png" width="1000px" title="The Default Cutter Interface Layout" alt="A screenshot of the default interface layout of Cutter.">
     <p class="two-no-mar centertext"><i>The default Cutter interface layout.</i></p>
     <p>At first, this interface looks very daunting and may be overwhelming. There is lots of information being displayed and many menus available, however in most cases only a few sections and tools are actually needed.</p>
     <p>The central panel with the tabs at the bottom is where most of your work will take place. The panels around the edge provide supporting information and other tools.</p>
 
     <h3 id="dashboard-overview">Dashboard</h3>
     <p>The dashboard tab contains an overview of the file that you are currently analysing, including the file format, size, architechture type and the libraries that it is using.</p>
-    <img class="radius-8" src="cutter-dashboard.png" width="1000px" title="The Cutter Dashboard" alt="A screenshot of the Cutter dashboard.">
+    <img class="radius-8" src="/blog/radare2-cutter-part-1-key-terminology-and-overview/cutter-dashboard.png" width="1000px" title="The Cutter Dashboard" alt="A screenshot of the Cutter dashboard.">
 
     <h3 id="disassembly-overview">Disassembly</h3>
     <p>The disassembly panel shows the disassembled machine code of the program. This is known as assembly language, and contains raw instructions such as <code>mov</code>, <code>push</code> and <code>call</code> and the arguments that go along with them.</p>
     <p>You can change the text size in the disassembly view using Ctrl + Shift + "+" (to increase) and Ctrl + "-" (to decrease).</p>
-    <img class="radius-8" src="cutter-disassembly.png" width="1000px" title="The Cutter Disassembly View" alt="A screenshot of the Cutter disassembly view.">
+    <img class="radius-8" src="/blog/radare2-cutter-part-1-key-terminology-and-overview/cutter-disassembly.png" width="1000px" title="The Cutter Disassembly View" alt="A screenshot of the Cutter disassembly view.">
     <p>In the x86_64 instruction set, there are a large number of unique instructions. The number varies depending on how you define an instruction, but it ranges from almost 1000 to significantly more than 1000. Stefan Heule has <a href="https://stefanheule.com/blog/how-many-x86-64-instructions-are-there-anyway/" target="_blank" rel="noopener"> an interesting article</a> on this if you're interested in how these numbers are calculated.</p>
     <p>In practise though, only a small subset of these instructions are frequently seen. After a short while you will easily pick up the top 20 or 30 instructions, and this is all you will need for most analysis tasks. I have included a list of 'popular' instructions below for reference:</p>
     <ul>
@@ -150,7 +150,7 @@ Part 1: Key Terminology and Overview</b>
 
     <h3 id="graph-overview">Graph</h3>
     <p>The graph view is used to visually display the process flow and execution paths available to the program. It's essentially a flowchart that maps out the program and all of the potential different ways that it can execute.</p>
-    <img class="radius-8" src="cutter-graph.png" width="1000px" title="The Cutter Graph View" alt="A screenshot of the Cutter graph view.">
+    <img class="radius-8" src="/blog/radare2-cutter-part-1-key-terminology-and-overview/cutter-graph.png" width="1000px" title="The Cutter Graph View" alt="A screenshot of the Cutter graph view.">
     <p>For example, in the event of a <code>cmp</code> (Compare) followed by one of the various jump instructions (which could be equivalent to an <code>if</code> statement in a higher-level language for simplicity's sake), there would be two arrows coming out and pointing to different parts of the program. One of these parts is what is executed if the <code>if</code> statement returns true, and the other would be for if it returns false.</p>
     <p>The arrows are simply visual representations of the various jump instructions, such as <code>jmp</code>, <code>jne</code> or <code>je</code>. The green arrow shows what happens if the jump takes place, and the orange arrow shows what happens if it doesn't. Grey arrows show a loop.</p>
     <p>The graph view can be moved around by clicking and dragging, and zoomed using Ctrl + Scroll Wheel. Double-clicking on any jump within the graph view will take you to the destination, and double-clicking an address will take you to that address in the disassesmbly view.</p>
@@ -158,7 +158,7 @@ Part 1: Key Terminology and Overview</b>
     <h3 id="functions-overview">Functions</h3>
     <div class="display-flex">
         <div>
-            <img class="radius-8" src="cutter-functions.png" width="300px" title="The Cutter Functions List" alt="A screenshot of the Cutter functions list.">
+            <img class="radius-8" src="/blog/radare2-cutter-part-1-key-terminology-and-overview/cutter-functions.png" width="300px" title="The Cutter Functions List" alt="A screenshot of the Cutter functions list.">
         </div>
         <div class="display-flex flex-direction-column">
             <p class="no-mar-top margin-left-24">On the left hand side of the default interface layout, there is the functions list. This shows a list of functions that Cutter has been able to detect in the program.</p>
@@ -169,7 +169,7 @@ Part 1: Key Terminology and Overview</b>
 
     <h3 id="strings-overview">Strings</h3>
     <p>The strings view shows a stringdump of the binary that you are analysing. A stringdump shows text strings that have been found within the binary.</p>
-    <img class="radius-8" src="cutter-strings.png" width="1000px" title="The Cutter Strings View" alt="A screenshot of the Cutter strings view.">
+    <img class="radius-8" src="/blog/radare2-cutter-part-1-key-terminology-and-overview/cutter-strings.png" width="1000px" title="The Cutter Strings View" alt="A screenshot of the Cutter strings view.">
     <p>A stringdump will often give you a lot of clues about what the functionality and purpose of the binary is. Stringdump results are often your first lead when starting to analyse a new binary.</p>
     <p>When analysing malware, strings may have been placed in order to throw you off-track, so make sure to keep this in mind.</p>
 
@@ -181,13 +181,13 @@ Part 1: Key Terminology and Overview</b>
             <p class="no-mar-top">However, when analysing malware, it is important to keep in mind that malware authors often try to hide their code within standard libraries in order to make it more difficult to find using static analysis. It could be that <code>main</code> contains only legitimate/safe code, while the malware is actually hidden in a function from a standard library that you would usually ignore at first.</p>
         </div>
         <div>
-            <img class="radius-8 margin-left-24" src="cutter-sidebar.png" width="298px" title="The Cutter Sidebar" alt="A screenshot of the Cutter sidebar.">
+            <img class="radius-8 margin-left-24" src="/blog/radare2-cutter-part-1-key-terminology-and-overview/cutter-sidebar.png" width="298px" title="The Cutter Sidebar" alt="A screenshot of the Cutter sidebar.">
         </div>
     </div>-->
 
     <h3 id="hexdump-overview">Hexdump</h3>
     <p>The hexdump view shows a copy of the binary you are analysing in hexadecimal (base 16) form.</p>
-    <img class="radius-8" src="cutter-hexdump.png" width="1000px" title="The Cutter Hexdump View" alt="A screenshot of the Cutter hexdump view.">
+    <img class="radius-8" src="/blog/radare2-cutter-part-1-key-terminology-and-overview/cutter-hexdump.png" width="1000px" title="The Cutter Hexdump View" alt="A screenshot of the Cutter hexdump view.">
     <p>The first column is the offset, the second column is the hexadecimal output, and the third is the ASCII representation of the data.</p>
     <p>The hex output is exactly the same as what you will get from the <code>hexdump -vC</code> command:</p>
     <pre>js@box:~$ hexdump -vC example.txt
@@ -199,7 +199,7 @@ Part 1: Key Terminology and Overview</b>
 
     <h3 id="pseudocode-overview">Pseudocode</h3>
     <p>Cutter will attempt to display a pseudocode representation of the disassembled binary.</p>
-    <img class="radius-8" src="cutter-pseudocode.png" width="1000px" title="The Cutter Pseudocode View" alt="A screenshot of the Cutter pseudocode view.">
+    <img class="radius-8" src="/blog/radare2-cutter-part-1-key-terminology-and-overview/cutter-pseudocode.png" width="1000px" title="The Cutter Pseudocode View" alt="A screenshot of the Cutter pseudocode view.">
     <p>The indentation and formatting is often off though, which can make it confusing at first glance. In the example below, the way that the destination of the <code>goto</code> is represented as a <code>do</code> could be misleading at first. Additionally, you could mistake the <code>while</code> loop as being part of the <code>if</code> statement, whereas it really comes after the <code>do</code>:</p>
     <pre>...
 r12d = 0
@@ -239,21 +239,21 @@ return;
 
     <h3 id="entrypoints-overview">Entry Points</h3>
     <p>Cutter will detect and display the entry points of the program.</p>
-    <img class="radius-8" src="cutter-entrypoints.png" width="1000px" title="The Cutter Entry Points View" alt="A screenshot of the Cutter entry points view.">
+    <img class="radius-8" src="/blog/radare2-cutter-part-1-key-terminology-and-overview/cutter-entrypoints.png" width="1000px" title="The Cutter Entry Points View" alt="A screenshot of the Cutter entry points view.">
     <p>Entry points are where control is passed from the operating system to the program. Essentially it's the location in the binary that will be executed first when it is run.</p>
 
     <h3 id="imports-overview">Imports</h3>
     <p>The imports view displays a list of libraries that are imported by the binary that you are analysing.</p>
-    <img class="radius-8" src="cutter-imports.png" width="1000px" title="The Cutter Imports View" alt="A screenshot of the Cutter imports view.">
+    <img class="radius-8" src="/blog/radare2-cutter-part-1-key-terminology-and-overview/cutter-imports.png" width="1000px" title="The Cutter Imports View" alt="A screenshot of the Cutter imports view.">
 
     <h3 id="symbols-overview">Symbols</h3>
     <p>The symbols view shows a list of the symbols from the symbol table of the binary that you analysing.</p>
-    <img class="radius-8" src="cutter-symbols.png" width="1000px" title="The Cutter Symbols View" alt="A screenshot of the Cutter symbols view.">
+    <img class="radius-8" src="/blog/radare2-cutter-part-1-key-terminology-and-overview/cutter-symbols.png" width="1000px" title="The Cutter Symbols View" alt="A screenshot of the Cutter symbols view.">
     <p>The symbol table contains information on various elements of the program, such as funtion names and entry points.</p>
 
     <h3 id="jupyter-overview">Jupyter Notebook</h3>
     <p>An additional feature of Cutter is the integrated Juptyer notebook.</p>
-    <img class="radius-8" src="cutter-jupyter.png" width="1000px" title="The Cutter Jupyter View" alt="A screenshot of the Cutter Jupyter view.">
+    <img class="radius-8" src="/blog/radare2-cutter-part-1-key-terminology-and-overview/cutter-jupyter.png" width="1000px" title="The Cutter Jupyter View" alt="A screenshot of the Cutter Jupyter view.">
     <p>The Juptyer notebook is a server-based notebook application that supports both rich text and computer code. It allows you write and run Python directly in your notebook documents, which could be useful for many reverse engineering tasks.</p>
     <p>I personally have not used the Jupyter notebook feature very much in Cutter, so I'm not aware of all of the features and whether it is useful or not.</p>
 
