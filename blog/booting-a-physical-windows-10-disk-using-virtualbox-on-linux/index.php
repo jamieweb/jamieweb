8f82b72b1d75ded0b53adba4c505694a0398d71c
Jamie Scaife <jamie@jamieweb.net>
2018-11-04 02:07:40 +0000

    Added info on determining block device ID, file system permissions

--- blog/booting-a-physical-windows-10-disk-using-virtualbox-on-linux/index.php
+++ blog/booting-a-physical-windows-10-disk-using-virtualbox-on-linux/index.php
@@ -35,8 +35,46 @@
 &#x2523&#x2501&#x2501 <a href="#apache-vhost">Apache Web Server Virtual Host Configuration</a>
 &#x2523&#x2501&#x2501 <a href="#problems">Problems With Namecoin</a>
 &#x2517&#x2501&#x2501 <a href="#conclusion">Conclusion</a></pre>
-    <h2 id="initial-setup">
-    <p>If you notice any mistakes, errors or 404 links, please let me know. Thanks!</p>
+    <h2 id="identifying-the-disk">Identifying the Disk</h2>
+    <p>Mounting the disk/partitions is not actually required for VirtualBox to be able to boot it, however you will need to identify the correct block device ID for the disk (e.g. <code>/dev/sdb</code>).</p>
+    <p>If you're using a modern desktop environment and file manager (e.g. XFCE with Thunar), the Windows 10 disk will probably be automatically mounted (if you get stuck at the error 'Windows is hibernated, refused to mount.', please see the troubleshooting <a href="#windows-is-hibernated-refused-to-mount" target="_blank">here</a>). In my case, the <code>Windows</code> and <code>Recovery Image</code> partitions were auto-mounted. Then, you can use <code>lsblk</code> (List Block Devices) to identify the correct device:</p>
+    <pre>jamie@box:~$ lsblk
+NAME           MAJ:MIN RM   SIZE RO TYPE  MOUNTPOINT
+sdb              8:16   0 931.5G  0 disk  
+├─sdb4           8:20   0   980M  0 part  
+├─sdb2           8:18   0   128M  0 part  
+├─sdb5           8:21   0  17.2G  0 part  /media/jamie/Recovery Image
+├─sdb3           8:19   0 912.9G  0 part  /media/jamie/Windows
+└─sdb1           8:17   0   360M  0 part  
+sda              8:0    0 111.8G  0 disk  
+├─sda2           8:2    0     1K  0 part  
+├─sda5           8:5    0   3.9G  0 part  
+│ └─cryptswap1 253:0    0   3.9G  0 crypt 
+└─sda1           8:1    0 107.9G  0 part  /</pre>
+    <p>From this, you can determine that <code>/dev/sdb</code> is the Windows 10 disk.</p>
+    <p><b>If your disk is not automatically mounted, you don't need to mount it.</b> You can use <code>lshw</code> to determine the correct device:</p>
+    <pre>jamie@box:~$ sudo lshw -short -class disk,volume
+H/W path               Device           Class          Description
+==================================================================
+/0/100/14/0/7/0.0.0    /dev/sdb         disk           1TB 2115
+/0/100/14/0/7/0.0.0/1  /dev/sdb1        volume         359MiB Windows FAT volume
+/0/100/14/0/7/0.0.0/2  /dev/sdb2        volume         127MiB reserved partition
+/0/100/14/0/7/0.0.0/3  /dev/sdb3        volume         912GiB Windows NTFS volume
+/0/100/14/0/7/0.0.0/4  /dev/sdb4        volume         979MiB Windows NTFS volume
+/0/100/14/0/7/0.0.0/5  /dev/sdb5        volume         17GiB Windows NTFS volume
+/0/1/0.0.0             /dev/sda         disk           120GB KINGSTON SV300S3
+/0/1/0.0.0/1           /dev/sda1        volume         107GiB EXT4 volume
+/0/1/0.0.0/2           /dev/sda2        volume         4008MiB Extended partition
+/0/1/0.0.0/2/5         /dev/sda5        volume         4008MiB Linux swap / Solaris partition</pre>
+    <p>From this output, you can see that <code>/dev/sdb</code> is the Windows 10 disk.</p>
+
+    <h2 id="file-system-permissions">File System Permissions</h2>
+    <p>In order to have full, unrestricted access to the Windows 10 disk without having to use <code>sudo</code>, you need to give your user account the appropriate permissions.</p>
+    <p>First, determine the group that has access to the drive. <b>Substitute <code>/dev/sdb</code> with the block device ID that you determined in the previous section:</b></p>
+    <pre>jamie@box:~$ ls -la /dev/sdb
+brw-rw---- 1 root disk 8, 16 Nov  3 23:36 /dev/sdb</pre>
+    <p>From this, you can determine that the group is <code>disk</code>, and that this group has read and write access to the block device (<code>brw-<b>rw-</b>---</code>).</p>
+    <p>The group name and permissions may differ for you, however in many cases it will be as above.</p>
 </div>
 
 <?php include "footer.php" ?>
