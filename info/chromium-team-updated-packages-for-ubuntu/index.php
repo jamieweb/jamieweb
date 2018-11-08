2bd3ac283d8ae5162e33de08413cb6ec5f640d20
Jamie Scaife <jamie@jamieweb.net>
2018-11-08 23:19:10 +0000

    Completed chromium-team page

--- info/chromium-team-updated-packages-for-ubuntu/index.php
+++ info/chromium-team-updated-packages-for-ubuntu/index.php
@@ -21,14 +21,14 @@
     <h1>Updated Chromium Browser Packages for Ubuntu</h1>
     <hr>
     <h2>The Problem</h2>
-    <p>In Ubuntu, the <code>chromium-browser</code> package is in the Canonical Universe repository, which means that it is community-mainted rather than officially supported by Canonical.</p>
-    <p>The result of this is that updates to <code>chromium-browser</code> are often not promptly released, leaving you running an older version than the latest stable release.</p>
+    <p>In Ubuntu, the <code>chromium-browser</code> package is in the Canonical Universe repository, which means that it is community-maintained rather than officially supported by Canonical.</p>
+    <p>The result of this is that the <code>chromium-browser</code> package is sometimes out of date, leaving you running an older version of Chromium Browser rather than the latest stable release.</p>
     <h2>The Solution</h2>
-    <p>The Launchpad PPA <code>chromium-team</code> contains Chromium Browser packages that are updated roughly in-line with the main Google Chrome release schedule. The <code>stable</code>, <code>beta</code> and <code>dev</code> branches are available.</p>
+    <p>The Launchpad PPA <code>chromium-team</code> contains Chromium Browser packages that are updated more regularly. The <code>stable</code>, <code>beta</code> and <code>dev</code> branches are available.</p>
     <p>You can add this PPA to your system by first ensuring that the <code>software-properties-common</code> package is installed, then running the following command (for whicver branch you want):</p>
     <pre>$ sudo apt-add-respository ppa:chromium-team/stable</pre>
-    <h2>Is the chromium-team PPA Official and Legitimate?</h2>
-    <p></p>
+    <h2>Is the chromium-team repository safe to use?</h2>
+    <p>The <code>chromium-browser</code> packages in the <code>chromium-team</code> PPA are primarily maintained by Oliver Tilloy of Canonical. The PPA is also referenced in the official Chromium repository in <code><a href="https://chromium.googlesource.com/chromium/src/+/HEAD/docs/linux_chromium_packages.md" target="_blank" rel="noopener">docs/linux_chromium_packages.md</a></code>.</p>
 </div>
 
 <?php include "footer.php" ?>
