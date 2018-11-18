9235a8a05b736a902c4ba8426e15bbd2fb12f4d1
Jamie Scaife <jamie@jamieweb.net>
2018-11-18 00:43:47 +0000

    Added test include of head.php for head meta tags

---
+++ head.php
@@ -0,0 +1,4 @@
+<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="Jamie, Scaife, jamie scaife, jamiescaife, jamieonubuntu, jamie90437, jamie90437x, jamieweb, jamieweb.net">
+    <meta name="author" content="Jamie Scaife">
+    <link href="/jamie.css" rel="stylesheet">
