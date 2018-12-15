fd098bd76c290690ce1241f406a2d6550856f959
Jamie Scaife <jamie@jamieweb.net>
2018-12-15 22:39:49 +0000

    Added initial starting point for bloom filter article

---
+++ blog/using-a-bloom-filter-to-anonymize-web-server-logs/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!--Copyright Jamie Scaife-->
+<!--Legal Information at https://www.jamieweb.net/contact-->
+
+<head>
+    <title>Using a Bloom Filter to Anonymize Web Server Logs</title>
+    <meta name="description" content="Anonymizing personal data in web server access logs in order to improve data security and comply with the GDPR.">
+    <?php include "head.php" ?>
+    <link href="https://www.jamieweb.net/blog/using-a-bloom-filter-to-anonymize-web-server-logs/" rel="canonical">
+</head>
+
+<body>
+
+<?php include "navbar.php" ?>
+
+<div class="body">
+    <h1>Using a Bloom Filter to Anonymize Web Server Logs</h1>
+    <hr>
+    <p><b>Monday 17th December 2018</b></p>
+    <p></p>
+</div>
+
+<?php include "footer.php" ?>
+
+</body>
+
+</html>
