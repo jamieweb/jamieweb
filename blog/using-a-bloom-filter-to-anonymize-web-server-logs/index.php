a71bd289f6a1f091f5926af9c2df38743031d3fd
Jamie Scaife <jamie@jamieweb.net>
2018-12-16 19:21:47 +0000

    Completed further sections of bloom filter article, added image of bloom filter calculator

--- blog/using-a-bloom-filter-to-anonymize-web-server-logs/index.php
+++ blog/using-a-bloom-filter-to-anonymize-web-server-logs/index.php
@@ -27,17 +27,22 @@
     <pre><b>Using a Bloom Filter to Anonymize Web Server Logs</b>
 &#x2523&#x2501&#x2501 <a href="#overview-of-the-tool">Overview of the Tool</a>
 &#x2523&#x2501&#x2501 <a href="#what-is-a-bloom-filter">What is a bloom filter?</a>
-&#x2523&#x2501&#x2501 <a href="#finding-the-perfect-bloom-filter-configuration">Finding the Perfect Bloom Filter Configuration</a>
-&#x2523&#x2501&#x2501 <a href="#fuzzing-the-tool-with-radamsa">Fuzzing the Tool with Radamsa</a>
+&#x2523&#x2501&#x2501 <a href="#finding-the-optimal-bloom-filter-configuration">Finding the Optimal Bloom Filter Configuration</a>
+&#x2523&#x2501&#x2501 <a href="#fuzz-testing-with-radamsa">Fuzz Testing with Radamsa</a>
 &#x2523&#x2501&#x2501 <a href="#implementing-the-tool">Implementing the Tool</a>
 &#x2523&#x2501&#x2501 <a href="#gdpr">GDPR</a>
 &#x2517&#x2501&#x2501 <a href="#conclusion">Conclusion</a></pre>
 
     <h2 id="overview-of-the-tool">Overview of the Tool</h2>
-    <p>The tool is written in Python 3, and is capable of processing web server access logs produced by web servers such as Apache or nginx.</p>
+    <p>The tool is written in Python 3, and is capable of anonymizing web server access logs produced by web servers such as Apache or nginx.</p>
+    <p>If you run the following log entry through the tool:</p>
+    <pre>127.0.0.1 - - [16/Dec/2018:16:07:23 +0000] "GET /pages/page1.html?query=string&query2=string2 HTTP/1.1" 200 11576 "https://www.example.com/path/file.html?query=string&query2=string2#fragment1" "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/71.0.3578.80 Chrome/71.0.3578.80 Safari/537.36"</pre>
+    <p>...the anonymized output will be:</p>
+    <pre>192.0.2.1 - - [16/Dec/2018:16:07:23 +0000] "GET /pages/page1.html HTTP/1.1" 200 11576 "https://www.example.com" "Chromium"</pre>
     <p>A bloom filter is used to keep track of which IP addresses have been seen before. This is what allows the outputted log files to be used for counting unique visitor IPs. Other anonymization solutions rely on obfuscating, invalidating or removing IP addresses, which usually limits or prevents them being used for this purpose.</p>
     <p>User agent strings are compared against approved lists of browsers and web crawlers, and replaced accordingly. The output log files will contain only the browser name, such as <code>Chrome</code> or <code>Firefox</code>, rather than the full user agent string including version numbers, etc.</p>
     <p>Referrer URLs are stripped down to only the scheme (such as <code>https</code>) and Fully Qualified Domain Name (such as <code>www.jamieweb.net</code>).</p>
+    <p>The HTTP Basic Authentication user ID and the <a href="https://tools.ietf.org/html/rfc1413" target="_blank" rel="noopener">RFC1413</a> identity (Ident protocol) are completely discarded. These aren't used very widely anyway.</p>
     <p>Please see the <a href="https://gitlab.com/jamieweb/web-server-log-anonymizer-bloom-filter/blob/master/README.md" target="_blank" rel="noopener">README</a> for the tool in the GitLab repository for further details.</p>
     <p>The anonymized logs can be fed into a statistics tool such as AWStats. Of course an amount of accuracy and detail is lost during the anonymization process, however key information such as the number of unique visitors, page views and referring sites is still available. This is a fantastic balance between using an intrusive, JavaScript-heavy client-side tracking and having no insight at all.</p>
 
@@ -46,7 +51,7 @@
     <p>They consist of an array of bits, and can be either written to, or queried. If you want to add a piece of data to a bloom filter, hashes of the data are generated, and the corresponding bits in the bloom filter are set to <code>1</code>. Full, cryptographically secure hashes are not normally used - instead, they are split into segments (such as 5 characters at a time), or other algorithms such as murmur are used.</p>
     <p>For example, imagine a brand new bloom filter which is 16 bits in size:</p>
     <pre>0000000000000000</pre>
-    <p>If we want to add the data <code>helloworld</code> to the bloom filter, we must first generate a hash of the data. The maximum potential value of the hash should be the same size as the bloom filter. In this case, the bloom filter is 16 bits in size, so the maximum value of the hash can be 16.</p>
+    <p>If we want to add the data <code>helloworld</code> to the bloom filter, we must first generate a hash of the data. The maximum potential value of the hash should be the same size as the bloom filter. In this case, the bloom filter is 16 bits in size, so the maximum value of the hash can be 16:</p>
     <pre>$ echo "helloworld" | sha256sum
 8cd07f3a5ff98f2a78cfc366c13fb123eb8d29c1ca37c79df190425d5b9e424d  -</pre>
     <p>The SHA-256 hash value of <code>helloworld</code> is obviously significantly larger than 16 when converted to base 10, so we need to cut it down to an appropriate size. SHA-256 hashes are represented in hexadecimal (base 16), meaning that each character can represent the values 0 to 15 in base 10.</p>
@@ -78,18 +83,70 @@
         ^   ^</pre>
     <p>Both of the required bits are set, so this proves that the data <i>might</i> be present in the filter.</p>
     <p>Bloom filters have a non-zero inaccuracy rate. Querying a filter will give you either one of two outputs: that the data <i>might</i> exist, or that the data definitely doesn't exist. In other words, false positives are possible, but false negatives are impossible. This doesn't make them useless though, as there are many use cases where 100% accuracy is not required.</p>
-    <p>The most well-known use of bloom filters is performance and efficiency. Querying large databases or other sets of data directly can take a large amount of computer processing power and time. Having a bloom filter for the data set and querying that first can help to narrow down the search, potentially resulting in huge performance increases for the system.</p>
+    <p>The most well-known use of bloom filters is performance and efficiency. Querying large databases or other sets of data directly can take a large amount of computer processing power and time, so having a bloom filter for the data set and querying that first can help to narrow down the search, potentially resulting in huge performance increases for the system.</p>
 
-    <h2 id="finding-the-perfect-bloom-filter-configuration">Finding the Perfect Bloom Filter Configuration</h2>
+    <h2 id="finding-the-optimal-bloom-filter-configuration">Finding the Optimal Bloom Filter Configuration</h2>
+    <p>One of the first challenges that I faced when starting this project was in finding the most optimal bloom filter configuration. I needed a bloom filter that is accurate, but not <i>too</i> accurate, and can also store a large amount of entries.</p>
+    <p>However, the most important factor in this case is security, so above all I needed a bloom filter that would be resistant to brute-force attacks should somebody gain unauthorized access to it.</p>
+    <p>The term 'brute-force attack' may sound unusual when talking about a bloom filter, however in this particular case it is a very real threat. Since the filter will be storing IP addresses, the data is very predictable. There are only a total of 3,706,452,992 (3.7 billion) publicly routable IPv4 addresses available, which may sound like a large number, but in reality it would only take a few seconds for a computer to query the bloom filter with each one of these. For IPv6, it would take much longer, but the risk is still there.</p>
+    <p>In order to eliminate this risk, the bloom filter needs to be inaccurate enough that it's not possible to uniquely identify a single IP address. In other words, there needs to be a certain amount of <a href="https://en.wikipedia.org/wiki/K-anonymity" target="_blank" rel="noopener"><i>k</i>-anonymity</a> for IP addresses stored in the filter. This is achieved by using a filter that is smaller than the total number of possible IP addresses, and using a total combined hash length that doesn't allow IP addresses to be uniquely identified.</p>
+    <p>As I discussed in the previous section, the size of the bloom filter should be tied to the maximum possible value of a single input hash segment. This means that we need to find a segment length which provides an adequate level of inaccuracy when used with IPv4 addresses.</p>
+    <p>Let's try 6 characters, where the maximum potential value (ffffff) is 16,777,215 (16.7 million). Divide this number by the total number of publicly routable IPv4 addresses:</p>
+    <pre>3706452992 / 16777215 = 220.921827133</pre>
+    <p>This means that on average, each 6 character hash segment will represent ~221 unique publicly routable IPv4 addresses. In other words, if you were to brute-force the bloom filter, there would be an average of 221 IP addresses that match every entry, which prevents you from accurately identifying a single unique one.</p>
+    <p>It's up to you whether ~221-anonymity is an acceptable amount, however for my system I decided to go with 5 character hash segments. The maximum potential value (fffff) is 1,048,575, so let's divide this by the total number of publicly routable IPv4 addresses:</p>
+    <pre>3706452992 / 1048575 = 3534.75239444</pre>
+    <p>This results in ~3535-anonymity, which I believe is a much more acceptable value for my particular setup.</p>
+    <p>For this sort of use, it's best to only use a single hash segment with the bloom filter. Using multiple would effectively increase the length of the hash used to identify unique IP addresses, causing the <i>k</i>-anonymity value to be either significantly reduced or completely eliminated.</p>
+    <p>Based on these calculations, the bloom filter configuration that I decided to go with for my own system is:</p>
+    <ul>
+        <li><b>Size:</b> 1,048,575 bits (128 KiB)</li>
+        <li><b>Hash Segment Length:</b> 5 characters (1,048,575 possible unique values)</li>
+        <li><b>Hash Segment Count:</b> 1</li>
+    </ul>
+    <p>These values can be easily controlled in the configuration file for the tool.</p>
+    <p>I also recommend having a look at the <a href="https://hur.st/bloomfilter/" target="_blank" rel="noopener">Bloom Filter Calculator</a> on <a href="https://hur.st/" target="_blank" rel="noopener">hur.st</a>, as this is an extremely useful tool for testing different configurations and seeing the properties that they have.</p>
+    <img class="radius-8" src="/blog/using-a-bloom-filter-to-anonymize-web-server-logs/bloom-filter-calculator.png" width="1000px" title="Bloom Filter Calculator on hur.st" alt="A screenshot of the bloom filter calculator on hur.st.">
 
-    <h2 id="fuzzing-the-tool-with-radamsa">Fuzzing the Tool</h2>
+    <h2 id="fuzz-testing-with-radamsa">Fuzz Testing with Radamsa</h2>
+    <p>I've written a basic fuzzing test harness for the tool in order to assist with identifying any potential vulnerabilities. My fuzzing engine of choice is <a href="https://gitlab.com/akihe/radamsa" target="_blank" rel="noopener">Radamsa</a>, however you should be able to swap in a different tool if you prefer.</p>
+    <p>You'll probably have to adjust the file paths to get this working on your system, but other than that it should work as-is:</p>
+    <pre>#!/bin/bash
+echo "Please ensure that you run this fuzzing script on a tmpfs/ramdisk, as the high-speed and repeated writes may damage your hard disk or SSD."
+while true; do
+    radamsa rawlogs/radamsa-sample.log > rawlogs/raw-access.log-input
+    output=$(python3 /home/bloomfilter/bloomfilter/web-server-log-anonymizer.py 2>&1)
+    if [[ $(echo "$output" | tail -n 1) != "Saving..." ]]; then
+        #Write crash log to file:
+        printf "**Found crash at `date`.**\n\nError output:\n" >> fuzz-output.log
+        echo "$output" >> fuzz-output.log
+        printf "\nFuzzed input:\n" >> fuzz-output.log
+        cat rawlogs/raw-access.log-input >> fuzz-output.log
+        printf "\n\n" >> rawlogs/fuzz-output.log >> fuzz-output.log
+        #Write summary to stdout:
+        printf "**Found crash at `date`. Last 10 lines of output:**\n\n"
+        echo "$output" | tail -n 10
+        printf "\n\n"
+    fi
+    rm outputlogs/access.log
+done</pre>
+    <p><b>Please ensure that you run this fuzzing script on a tmpfs/ramdisk, as the high-speed and repeated writes may damage your hard disk or SSD.</b> The below command will create a 128 MB tmpfs for you to use:</p>
+    <pre>sudo mount -t tmpfs -o size=128m tmpfs /path/to/desired/mount/directory</pre>
+    <p>Then make sure that all of the files the script needs to read and write are on the tmpfs.</p>
+    <p>I've run this fuzzing script for around 12 hours so far. It found a couple of crashes within 10 minutes, which I've now fixed, but other than that nothing has been found. Please feel free to perform more of your own testing and let me know if you find anything.</p>
 
     <h2 id="implementing-the-tool">Implementing the Tool</h2>
+    <p>I've implemented the tool on a dedicated Raspberry Pi Zero W logging device. Every hour, the logs are transferred from my main web server to the Raspberry Pi using SFTP, where they are then anonymized.</p>
+    <p>The un-anonymized logs are removed from disk on the server just a few minutes after they are transferred to the Pi, and on the Pi itself they are erased as soon as they have been processed.</p>
+    <p>Finally, the anonymized data is fed into AWStats in order to give me a high-level statistical insight into the traffic on my site.</p>
+    <p>All of this is set up with cronjobs and a restricted service account for SFTP.</p>
 
     <h2 id="gdpr">GDPR</h2>
     <p>While it's true that <a href="https://www.privacy-regulation.eu/en/recital-49-GDPR.htm" target="_blank" rel="noopener">Recital 49</a> of the GDPR allows for the processing of personal data (such as log files) in order to ensure sufficient levels of network and data security, this particular clause does not expand to statistical and analytical purposes.</p>
 
     <h2 id="conclusion">Conclusion</h2>
+    <p>Overall I am very happy with the outcome of this project, and I believe that I've struck a good balance between more traditional 'tracking' methods, and having no statistical insight at all.</p>
+    <p>I definitely don't believe that what I am doing is anywhere near what would traditionally be called 'tracking', and it's not even 'analytics' really as detailed usage patterns cannot be easily identified in the anonymized data. The new statistics which I have access to will allow me to assess the performance of my content and hopefully help with growing the site accordingly.</p>
     
 </div>
 
