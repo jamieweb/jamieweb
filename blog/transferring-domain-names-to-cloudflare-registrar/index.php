8654bab58859d2e89ae83eacf4d0e6a610ad50d3
Jamie Scaife <jamie@jamieweb.net>
2018-12-22 22:26:35 +0000

    New empty post for Cloudflare Registrar article

---
+++ blog/transferring-domain-names-to-cloudflare-registrar/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!--Copyright Jamie Scaife-->
+<!--Legal Information at https://www.jamieweb.net/contact-->
+
+<head>
+    <title>Transferring Domain Names to Cloudflare Registrar</title>
+    <meta name="description" content="Documentation on the transfer-in process for Cloudflare Registrar.">
+    <?php include "head.php" ?>
+    <link href="https://www.jamieweb.net/blog/transferring-domain-names-to-cloudflare-registrar/" rel="canonical">
+</head>
+
+<body>
+
+<?php include "navbar.php" ?>
+
+<div class="body">
+    <h1>Transferring Domain Names to Cloudflare Registrar</h1>
+    <hr>
+    <p><b>Sunday 23rd December 2018</b></p>
+    <p></p>
+</div>
+
+<?php include "footer.php" ?>
+
+</body>
+
+</html>
