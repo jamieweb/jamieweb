26a7c52493288fcc1378dfd0ebdb74c378c8583e
Jamie Scaife <jamie@jamieweb.net>
2018-12-24 00:36:43 +0000

    Removed response-headers.php include from 301 redirects

--- other/adblock-plus-config/index.php
+++ other/adblock-plus-config/index.php
@@ -1,3 +1,2 @@
-<?php include "response-headers.php"; content_security_policy(); ?>
 <?php header("HTTP/1.1 301 Moved Permanently");
 header("Location: /info/adblock-plus-config/"); ?>
--- other/chrome-extension-ids/index.php
+++ other/chrome-extension-ids/index.php
@@ -1,3 +1,2 @@
-<?php include "response-headers.php"; content_security_policy(); ?>
 <?php header("HTTP/1.1 301 Moved Permanently");
 header("Location: /info/chrome-extension-ids/"); ?>
