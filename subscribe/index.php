803bb2597ff8a4eb1a674a382e7dd20d47afbe75
Jamie Scaife <jamie@jamieweb.net>
2018-12-30 23:44:34 +0000

    Adjusted subscribe page to use new icon styling

--- subscribe/index.php
+++ subscribe/index.php
@@ -20,14 +20,8 @@
     <h1>Subscribe</h1>
     <hr>
     <p class="two-mar-bottom">Subscribe to receive notifications when I post new content to this blog. Usually there will be only a couple of notifications per month.</p>
-    <div class="icontext two-mar-top margin-right-20">
-        <a href="/rss.xml" target="_blank"><img src="/images/fontawesome/rss-orange.svg"></a>
-        <h4><a href="/rss.xml" target="_blank">RSS Feed</a></h4>
-    </div>
-    <div class="icontext two-mar-bottom">
-        <a href="https://feedly.com/i/subscription/feed%2Fhttps%3A%2F%2Fwww.jamieweb.net%2Frss.xml" target="_blank" rel="noopener"><img src="/images/fy.png"></a>
-        <h4><a href="https://feedly.com/i/subscription/feed%2Fhttps%3A%2F%2Fwww.jamieweb.net%2Frss.xml" target="_blank" rel="noopener">Feedly</a></h4>
-    </div>
+    <a class="icontext margin-top-7 margin-right-20" href="/rss.xml" target="_blank"><img src="/images/fontawesome/rss-orange.svg"> <span><b>RSS Feed</b></span></a>
+     <a class="icontext two-mar-bottom" href="https://feedly.com/i/subscription/feed%2Fhttps%3A%2F%2Fwww.jamieweb.net%2Frss.xml" target="_blank" rel="noopener"><img src="/images/fy.png"> <span><b>Feedly</b></span></a>
     <div class="email-form">
         <p class="two-no-mar">Or subscribe via email:</p>
         <form class="display-flex" action="https://www.getrevue.co/profile/jamieweb/add_subscriber" method="post" target="_blank">
