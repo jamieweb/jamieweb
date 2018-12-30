02e94efcb4bea283da44ab45034b8e96b1783d51
Jamie Scaife <jamie@jamieweb.net>
2018-12-30 16:16:53 +0000

    Added Feedly link to subscribe page

--- subscribe/index.php
+++ subscribe/index.php
@@ -19,11 +19,17 @@
 <div class="body">
     <h1>Subscribe</h1>
     <hr>
-        <p>Subscribe to receive notifications when I post new content to this blog. Usually there will be only a couple of notifications per month.</p>
-        <a class="display-inline-block" href="/rss.xml" target="_blank"><img class="icontext-img" src="/images/fontawesome/rss-orange.svg"></a>
-        <p class="display-inline-block no-mar-top no-mar-bottom"><a href="/rss.xml" target="_blank">RSS Feed</a></p>
+    <p class="two-mar-bottom">Subscribe to receive notifications when I post new content to this blog. Usually there will be only a couple of notifications per month.</p>
+    <div class="icontext two-mar-top margin-right-20">
+        <a href="/rss.xml" target="_blank"><img src="/images/fontawesome/rss-orange.svg"></a>
+        <h4><a href="/rss.xml" target="_blank">RSS Feed</a></h4>
+    </div>
+    <div class="icontext two-mar-bottom">
+        <a href="https://feedly.com/i/subscription/feed%2Fhttps%3A%2F%2Fwww.jamieweb.net%2Frss.xml" target="_blank" rel="noopener"><img src="/images/fy.png"></a>
+        <h4><a href="https://feedly.com/i/subscription/feed%2Fhttps%3A%2F%2Fwww.jamieweb.net%2Frss.xml" target="_blank" rel="noopener">Feedly</a></h4>
+    </div>
     <div class="email-form">
-        <p class="margin-top-19 no-mar-bottom">Or subscribe via email:</p>
+        <p class="two-no-mar">Or subscribe via email:</p>
         <form class="display-flex" action="https://www.getrevue.co/profile/jamieweb/add_subscriber" method="post" target="_blank">
             <input class="form-input margin-right-10 exempt" type="email" name="member[email]" placeholder="Your email address...">
             <input class="form-submit" type="submit" name="member[subscribe]" value="Go">
