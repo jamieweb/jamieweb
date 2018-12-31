abe3ce70dd46db8b97665f43a35a11948f6d5418
Jamie Scaife <jamie@jamieweb.net>
2018-12-31 00:17:40 +0000

    Adjusted FontAwesome license information

--- contact/index.php
+++ contact/index.php
@@ -47,17 +47,19 @@
     <p><b>Please note that my HackerOne program does not provide bounties/rewards as JamieWeb is only a small personal project.</b></p>
     <p>If you do not wish to use HackerOne to report a security vulnerability, please feel free to contact me using any other method.</p>
     <p>Thank you!</p>
-
     <hr>
+
     <h1 id="abuse">Network/Email Abuse</h1>
     <p>If you are experiencing any form of network abuse or unwanted email coming from my domain or systems, please contact me with details including applicable logs and/or headers.</p>
     <p>Any unwanted activity or abuse is most likely spoofed, as my systems do not make any unauthorized connections to any third-party systems or send any automated unsolicited email.</p>
 
     <hr>
     <h1 id="legal">Legal Information</h1>
-    <ul><li>Homepage sidebar logo: <b>Jdenticon by Daniel Mester Pirttij&#228;rvi</b> (<a href="https://jdenticon.com" target="_blank" rel="noopener">https://jdenticon.com</a>)</li>
-    <li>Icons around site: <b>Font Awesome by Dave Gandy</b> (<a href="https://fontawesome.com" target="_blank" rel="noopener">https://fontawesome.com</a>) - Licensed Under <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">CC BY 4.0</a></li>
-    <li>Site-wide font: <b>Open Sans by Steve Matteson</b> (<a href="https://fonts.google.com/specimen/Open+Sans" target="_blank" rel="noopener">https://fonts.google.com/<wbr>specimen/Open+Sans</a>) - Licensed Under <a href="https://www.apache.org/licenses/LICENSE-2.0.html" target="_blank">Apache 2.0</a></li></ul>
+    <ul class="spaced-list">
+        <li>Homepage sidebar logo: <b>Jdenticon by Daniel Mester Pirttij&#228;rvi</b> (<a href="https://jdenticon.com" target="_blank" rel="noopener">https://jdenticon.com</a>)</li>
+        <li>Icons around site: <b>Font Awesome by Dave Gandy</b> (<a href="https://fontawesome.com" target="_blank" rel="noopener">https://fontawesome.com</a>) - SVG Files Licensed Under <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">CC BY 4.0</a> (<a href="https://fontawesome.com/license/free" target="_blank" rel="noopener">https://fontawesome.com/license/free</a>) - Non-substantial changes were made to some icons, including modifying the fill colour, inverting the image or changing the size.</li>
+        <li>Site-wide font: <b>Open Sans by Steve Matteson</b> (<a href="https://fonts.google.com/specimen/Open+Sans" target="_blank" rel="noopener">https://fonts.google.com/<wbr>specimen/Open+Sans</a>) - Licensed Under <a href="https://www.apache.org/licenses/LICENSE-2.0.html" target="_blank">Apache 2.0</a></li>
+    </ul>
     <p>All content found here that has no attribution is believed to be in the public domain.</p>
     <p>If you are the rightful owner of any of the content on this site, and you do not want it to be displayed or you require suitable credit, then please contact me and I will do whatever is needed either for the content to be removed or to provide credit where it is due.</p>
     <p>Although I do my best to verify its legitimacy, I am not responsible for any of the content/downloads on external sites that are linked here.</p>
