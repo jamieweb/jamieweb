78c01461cbca3a535c92a8d9cb9912d6eb9fcad2
Jamie Scaife <jamie@jamieweb.net>
2019-01-01 20:15:45 +0000

    Further work on Cutter 3

--- blog/radare2-cutter-part-3-solving-a-crackme-challenge/index.php
+++ blog/radare2-cutter-part-3-solving-a-crackme-challenge/index.php
@@ -85,7 +85,33 @@ Enter Password (or q to quit): Access Denied</pre>
     <p>This will send 10 Megabytes of <code>a</code> characters to the program. In my case, the program didn't crash - it successfully processed the data and then started looping infinitely as stdin had closed. If you are using a very old compiler or system, your results could differ, but for modern systems they probably won't.</p>
 
     <h2 id="analysing-the-program-in-cutter">Analysing the Program in Cutter</h2>
-    <p></p>
+    <p>Now that we've built some initial high-level insight into how the program roughly works, you can open it in Cutter and start having a look around:</p>
+    <img class="radius-8" src="cutter-file-opened-disassembly-view-main.png" width="1000px" alt="A screenshot of the Cutter interface after opening the file, showing the disassembly view of the 'main' function.">
+    <p>The ultimate goal of analysing the program in Cutter is to either find the password, or find how the password is determined or generated. The password could be stored as a raw string, it could be encoded in an arbitrary format or it might instead be derived from the input rather than simply being a comparison against a stored value.</p>
+    <p>Before rushing straight into the assembly, we can have a look at the stringdump results, as this may give some initial clues:</p>
+    <img class="radius-8" src="cutter-stringdump.png" width="1000px" alt="A screenshot of the stringdump output in Cutter, showing some human-readable English strings related to the crackme challenge program.">
+    <p>As you can see, in the stringdump output there are some strings that stand out as particularly interesting or as human-readable English. Two of these we have already seen in the live program:</p>
+    <pre>Enter Password (or q to quit): 
+Access Denied</pre>
+    <p>However there are also two new ones:</p>
+    <pre>1568 1872 1632 1632 1616 1824 1776 1888 1616 1824 1632 1728 1776 1904 
+Access Granted</pre>
+    <p>The <code>Access Granted</code> string is probably what is output if the password is correct. We don't know for certain, but based entirely on context we can make this assumption for now. Some more advanced crackmes may try to throw you off with things like this, but this is only a beginner level challenge so that is unlikely.</p>
+    <p>However, what we're <i>really</i> interested in is the long string of numbers...</p>
+    <pre>1568 1872 1632 1632 1616 1824 1776 1888 1616 1824 1632 1728 1776 1904 </pre>
+    <p>Looking at the string the numerals 0 to 9 are included, as well as spaces at the end of each section (including the last one).</p>
+    <p>This is our first lead - it could be the encoded password, or it could be something completely unrelated, so investigating further is required in order to help prove this theory either way.</p>
+    <p>You can try double-clicking the interesting string in the stringdump view, but this doesn't always take you to a directly useful place in the program where the string is used.</p>
+    <p>Instead, let's have a proper look at <code>main</code>:</p>
+    <img class="radius-8" src="cutter-disassembly-crypto-function.png" width="1000px" alt="A screenshot of the disassembly view in Cutter, showing the 'main' function and some of the strings discovered earlier from the stringdump.">
+    <p>Scrolling down just slightly in the dissasembly view reveals some of the strings that we found earlier, most crucially the long list of decimal numbers. Since this is a basic program and we're looking at the strings in-use in <code>main</code>, it's highly likely that this is where the strings are actually used to perform a useful function in the program, rather than just being assigned or moved around at a low-level.</p>
+    <p>If you take a closer look, you may also notice a <code>call</code> instruction to an interesting function name; <code>crypto_std</code>. This function name stands out because cryptography functions wouldn't normally be used in very basic programs like this, unless they are actually doing something with cryprography.</p>
+    <p>In the case of this program, that could be the case. If the password is enciphered, the <code>crypto_std</code> funciton could well be the function responsible for performing the enciphering and deciphering. Let's take a closer look at it in the disassembly view:</p>
+    <img class="radius-8" src="cutter-crypto-shl.png" width="1000px" alt="A screenshot of the disassembly view in Cutter, showing the 'crypto' function with a 'shl' instruction highlighted.">
+    <p>While looking through the assembly, I noticed an <code>shl</code> instruction with a hard-coded operand of <code>4</code>. By 'hard-coded operand', I am referring to the fact that the second operand of the instruction is not a reference to a register, rather it is a static value that doesn't change throughout the execution of the program. This stood out as it is quite uncommon to see hard-coded operands in basic programs, espeically on less-common insutrctions such as <code>shl</code>.</p>
+    <p>The <code>shl</code> instruction is used to perform bitwise shifts, or 'bitshifts' as they are more commonly referred to. The Intel syntax for <code>shl</code> is <code>shl dest, src</code>, where <code>dest</code> is the data to perform the bitshift on, and <code>src</code> is the number of bits to shift the data by.</p>
+    <p>For example, <code>shl 01101100, 2</code> will result in <code>00011011</code>.</p>
+    <p>This instruction could be related to the password enciphering, so we should investigate it further.
 
 </div>
 
