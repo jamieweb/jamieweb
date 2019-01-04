8563d5281288929b984b852d7559c17163ed7304
Jamie Scaife <jamie@jamieweb.net>
2019-01-04 23:45:23 +0000

    Added meta generator tag with link to jamieweb/jamieweb on GitLab

--- includes/head.php
+++ includes/head.php
@@ -1,4 +1,5 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="keywords" content="Jamie, Scaife, jamie scaife, jamiescaife, jamieonubuntu, jamie90437, jamie90437x, jamieweb, jamieweb.net">
     <meta name="author" content="Jamie Scaife">
+    <meta name="generator" content="gitlab.com/jamieweb/jamieweb">
     <link href="/jamie.css" rel="stylesheet">
