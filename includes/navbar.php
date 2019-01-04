16000087bb5c4d8ef914f153f25e4e81dcec5496
Jamie Scaife <jamie@jamieweb.net>
2019-01-04 23:41:29 +0000

    Removed extra spaces in navbar code

--- includes/navbar.php
+++ includes/navbar.php
@@ -7,7 +7,7 @@
                 <h5><a href="/blog/"><span class="exempt">&#x1f58b;&#xfe0e;</span> <span>Blog</span></a></h5>
                 <div class="dropdown-content">
                     <h4>
-                        <?php include_once "bloglist.php"; bloglist("navbar"); ?>
+<?php include_once "bloglist.php"; bloglist("navbar"); ?>
                     </h4>
                 </div>
             </li>
