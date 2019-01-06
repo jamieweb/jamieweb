ffa8c4c680c399835782bcd16484d54ce58557ac
Jamie Scaife <jamie@jamieweb.net>
2019-01-06 23:55:15 +0000

    Added initial metadata for sponsor page

---
+++ sponsor/index.php
@@ -0,0 +1,29 @@
+<?php include "response-headers.php"; content_security_policy(); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<!--Copyright Jamie Scaife-->
+<!--Legal Information at https://www.jamieweb.net/contact-->
+
+<head>
+    <title>Sponsoring jamieweb.net</title>
+    <meta name="description" content="If you are interested in sponsoring jamieweb.net, this page contains everything you need to know.">
+    <?php include "head.php" ?>
+    <link href="https://www.jamieweb.net/sponsor/" rel="canonical">
+</head>
+
+<body>
+
+<?php include "navbar.php" ?>
+
+<div class="body">
+    <h1>Sponsoring jamieweb.net</h1>
+    <hr>
+    <p></p>
+</div>
+
+<?php include "footer.php" ?>
+
+</body>
+
+</html>
