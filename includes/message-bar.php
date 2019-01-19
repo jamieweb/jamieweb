1d4b2a9e6218939b6d6757c3d598c16e7b700d13
Jamie Scaife <jamie@jamieweb.net>
2019-01-19 02:39:03 +0000

    Updated sponsor message

--- includes/message-bar.php
+++ includes/message-bar.php
@@ -1,10 +1,10 @@
 <?php $schedule = [
     array (
-        "from" => "04 Jan 2019",
-        "to" => "05 Jan 2019",
-        "prefix" => "Currently sponsored by:",
-        "message" => "Example message.",
-        "href" => "/"
+        "from" => "19 Jan 2019",
+        "to" => "19 Jan 2020",
+        "prefix" => "Want to sponsor my site?",
+        "message" => "I am currently looking for sponsors for jamieweb.net - if you'd like to promote your brand or product via a sponsorship, please get in touch!",
+        "href" => "/sponsor/"
     ),
     array (
         "from" => "04 Jan 2019",
