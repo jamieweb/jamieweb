5b5fc62f6eee13abc38e55910ab73d1cafd4711a
Jamie Scaife <jamie@jamieweb.net>
2019-01-19 15:23:15 +0000

    Added hover text to emojis

--- includes/holiday-emojis.php
+++ includes/holiday-emojis.php
@@ -1,8 +1,8 @@
 <?php $cal = [
-    array ( "from" => "21 Apr", "to" => "22 Apr", "output" => "&#x1f423;" ), //Easter hatching chick
-    array ( "from" => "30 Oct", "to" => "02 Nov", "output" => "&#x1f383;" ), //Halloween pumpkin
-    array ( "from" => "19 Dec", "to" => "31 Dec", "output" => "&#x1f384;" ), //Christmas tree
-    array ( "from" => "31 Dec", "to" => "02 Jan next year", "output" => "&#x1f386;" ) //New year fireworks
+    array ( "from" => "21 Apr", "to" => "23 Apr", "output" => "<span title=\"Happy Easter!\">&#x1f423;</span>" ), //Easter hatching chick
+    array ( "from" => "30 Oct", "to" => "02 Nov", "output" => "<span title=\"Happy Halloween!\">&#x1f383;</span>" ), //Halloween pumpkin
+    array ( "from" => "19 Dec", "to" => "31 Dec", "output" => "<span title=\"Happy Holidays!\">&#x1f384;</span>" ), //Christmas tree
+    array ( "from" => "31 Dec", "to" => "02 Jan next year", "output" => "<span title=\"Happy New Year!\">&#x1f386;</span>" ) //New year fireworks
 ];
 $time = time();
 foreach($cal as $event) {
