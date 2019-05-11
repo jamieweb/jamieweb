07a4eaca1ea35dd40787f49d4f65a5e1fb204ea4
Jamie Scaife <jamie@jamieweb.net>
2019-05-12 00:23:00 +0100

    Added CiC podcast article

---
+++ blog/my-interview-with-giulio-d-agostino/index.php
@@ -0,0 +1,11 @@
+<?php include "response-headers.php"; content_security_policy();
+include_once "bloglist.php"; bloglist("postTop", null, null, 2019); ?>
+
+<div class="body">
+    <h1><?php echo $postInfo->title; ?></h1>
+    <hr>
+    <p><b><?php echo $postInfo->date; ?></b></p>
+    <p><?php echo $postInfo->snippet; ?></p>
+</div>
+
+<?php include "footer.php" ?>
