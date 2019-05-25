955cce3f895cc4253179b057505b3336575b7eca
Jamie Scaife <jamie@jamieweb.net>
2019-05-25 03:08:00 +0100

    Changed GitHub link to GitLab

--- projects/index.php
+++ projects/index.php
@@ -30,7 +30,7 @@
     <p class="two-mar-top"><b>An Internet Relay Chat bot that you can control into order to paint pixels on a canvas.</b></p>
 
     <br>
-    <p class="two-mar-top"><b>View additional projects on my GitHub profile: <a href="https://github.com/jamieweb" target="_blank" rel="noopener">https://github.com/jamieweb</a></b></p>
+    <p class="two-mar-top"><b>View additional projects on my GitLab profile: <a href="https://gitlab.com/jamieweb" target="_blank" rel="noopener">https://gitlab.com/jamieweb</a></b></p>
 </div>
 
 <?php include "footer.php" ?>
