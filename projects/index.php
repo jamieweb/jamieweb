0088e31795cc71f24c380f7b2be5840074c931c3
Jamie Scaife <jamie@jamieweb.net>
2019-09-08 00:06:40 +0100

    Initial work on updated projects page

--- projects/index.php
+++ projects/index.php
@@ -20,17 +20,38 @@
     <h1>Projects</h1>
     <hr>
 
-    <h3 class="no-mar-bottom"><a href="/projects/computing-stats/">Raspberry Pi + BOINC Stats</a></h3>
-    <p class="two-mar-top"><b>Stats from my Raspberry Pi Cluster and BOINC distributed computing.</b></p>
+    <h2 class="no-mar-bottom redlink"><a href="/projects/computing-stats/">Raspberry Pi Cluster</a></h2>
+    <p class="two-mar-top">A five-node Raspberry Pi Cluster running Einstein@Home and various other applications. It has been running 24/7 in my garage in one form or another since October 2015, which is over 30,000 computing hours as of September 2019. Live statistics are uploaded to this website every 10 minutes, including temperature, CPU and memory usage.</p>
+    <p>&#x1f4c8; <a href="/projects/computing-stats/">Live Stats</a> | &#x1f3a5; <a href="https://twitter.com/jamieweb/status/1062048785352871936" target="_blank" rel="noopener">Video</a></p>
+    <hr>
 
-    <h3 class="no-mar-bottom"><a href="/projects/chrome-site-whitelist-extension/">Chrome Site Whitelist Extension</a></h3>
-    <p class="two-mar-top"><b>Chrome extension that highlights links on Google/Reddit that have been whitelisted by the user.</b></p>
+    <h2 class="no-mar-bottom redlink"><a href="/" target="_blank" rel="noopener">DigitalOcean Tutorials</a></h2>
+    <p class="two-mar-top">I am a freelance technical writer for the cloud computing company DigitalOcean, where I write guides and tutorials about Linux, DNS, web technologies and more. I have covered topics including DNS infrastructure-as-code tools and email security. The payments for my work are kindly matched by DigitalOcean as a charitable donation to the Electronic Frontier Foundation.</p>
+    <p>&#x1f4dd; <a href="" target="_blank" rel="noopener">My DigitalOcean Tutorials</a></p>
+    <hr>
 
-    <h3 class="no-mar-bottom"><a href="/projects/irc-drawing-bot/">IRC Drawing Bot</a></h3>
-    <p class="two-mar-top"><b>An Internet Relay Chat bot that you can control into order to paint pixels on a canvas.</b></p>
+    <h2 class="two-mar-bottom redlink"><a href="https://gitlab.com/jamieweb/web-server-log-anonymizer-bloom-filter/" target="_blank" rel="noopener">Web Server Log File Anonymizer</a></h2>
+    <p class="two-mar-top">An anonymisation tool for web server log files which removes sensitive information such as IP addresses and user agents, while retaining the uniqueness of individual log entries. The tool utilises a Bloom filter in order to determine whether a particular IP address has been seen before, without actually having to permanently store it. A customisable level of <i>k</i>-anonymity is used to ensure that the filter cannot be brute-forced. This means that the resulting log files are still useful for high-level web statistics such as unique visitor counts, without having to store the potentially sensitive information that they usually contain.</p>
+    <p>&lt;/&gt; <a href="https://gitlab.com/jamieweb/web-server-log-anonymizer-bloom-filter/" target="_blank" rel="noopener">Source Code</a> | &#x1f4c3; <a href="/blog/using-a-bloom-filter-to-anonymize-web-server-logs/">Blog Post</a> | <> <a href="https://www.python.org/" target="_blank" rel="noopener">Python 3</a></p>
+    <hr>
+
+    <h2 class="two-mar-bottom redlink"><a href="https://gitlab.com/jamieweb/travis-ci_csp-tester/" target="_blank" rel="noopener">Content Security Policy Tester CI Pipeline Job</a></h2>
+    <p class="two-mar-top">A continuous integration job configuration for automatically testing for violations of your website's Content Security Policy. A copy of your PHP website with your desired CSP header is set up in the CI system and crawled using Headless Chrome Crawler, resulting in violation reports being sent to a local or remote reporting endpoint. The reports can then be reviewed in order to determine whether your site is compliant with its own security policy, helping to ensure that content will not be accidentally blocked.</p>
+    <p>&lt;/&gt; <a href="https://gitlab.com/jamieweb/travis-ci_csp-tester/" target="_blank" rel="noopener">Source Code</a> | &#x1f4c3; <a href="/blog/testing-your-csp-using-travis-ci-and-headless-chrome-crawler/">Blog Post</a> | <> <a href="https://yaml.org/" target="_blank" rel="noopener">YAML</a></p>
+    <hr>
+
+    <h2 class="two-mar-bottom redlink"><a href="https://gitlab.com/jamieweb/dl-integrity-verify/" target="_blank" rel="noopener">Automatic Software Package Integrity Verifier</a></h2>
+    <p class="two-mar-top">A Bash script to automatically download and perform GPG and/or hash integrity verifications on a list of pre-programmed software packages and files, including Ubuntu Linux installation ISOs and various Windows software. The purpose of this is to mitigate the risk of human error when carrying out this sensitive process, and to save time by making the process mostly automated.</p>
+    <p>&lt;/&gt; <a href="https://gitlab.com/jamieweb/dl-integrity-verify/" target="_blank" rel="noopener">Source Code</a> | &#x1f4c3; <a href="/blog/automating-the-integrity-verification-process-for-downloaded-software/">Blog Post</a> | <> <a href="https://www.gnu.org/software/bash/" target="_blank" rel="noopener">Bash</a></p>
+    <hr>
+
+    <h2 class="two-mar-bottom redlink"><a href="https://gitlab.com/jamieweb/results-whitelist/" target="_blank" rel="noopener">Link Whitelist Chrome Extension</a></h2>
+    <p class="two-mar-top">A Google Chrome extension which allows you to set a whitelist of trusted websites. Hyperlinks to these websites in Google search results, Reddit and Hacker News will then be highlighted in green. This is anti-typosquatting and anti-phishing tool designed to remove the requirement to carefully check links before clicking on them, and to provide assurance that the link you're clicking isn't a lookalike or phishing website.</p>
+    <p>&lt;/&gt; <a href="https://gitlab.com/jamieweb/results-whitelist/" target="_blank" rel="noopener">Source Code</a> | &#x1f4c3; <a href="/blog/chrome-site-whitelist-extension/">Blog Post</a> | <> <a href="https://en.wikipedia.org/wiki/JavaScript" target="_blank" rel="noopener">JavaScript</a></p>
+    <hr>
 
     <br>
-    <p class="two-mar-top"><b>View additional projects on my GitLab profile: <a href="https://gitlab.com/jamieweb" target="_blank" rel="noopener">https://gitlab.com/jamieweb</a></b></p>
+    <p class="two-mar-top"><b>View more of my projects and work on my <a href="/blog/">blog</a> and <a href="https://gitlab.com/jamieweb/" target="_blank" rel="noopener">GitLab profile</a>.</b></p>
 </div>
 
 <?php include "footer.php" ?>
