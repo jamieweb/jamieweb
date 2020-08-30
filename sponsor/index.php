3e67797a003ddfef7cd30360b0575328c48587a2
Jamie Scaife <jamie@jamieweb.net>
2020-08-30 23:36:57 +0100

    Updated wording and pricing on sponsor page

--- sponsor/index.php
+++ sponsor/index.php
@@ -23,13 +23,12 @@
     <p>A sponsorship provides the opportunity for you to promote your brand or product directly to between 7,000 and 8,000 unique human visitors per month.</p>
 
     <h2 id="pricing">Pricing</h2>
-    <p>One month of sponsorship currently costs <b>$20 US</b>, however this is up for negotiation.</p>
-    <p>If you wish to sponsor me for multiple months, there will be a discount. I can accept the following payment methods:</p>
+    <p>One month of sponsorship currently costs <b>$60 US</b>.</p>
+    <p>If you wish to sponsor me for multiple months, I may offer reduced rates. I can accept the following payment methods:</p>
     <ul>
         <li>UK bank transfer</li>
         <li>PayPal</li>
-        <li>Bitcoin (or other reputable cryptocurrency, upon agreement)</li>
-        <li>Other of your choice (if possible)</li>
+        <li>Other of your choice (upon agreement)</li>
     </ul>
     <p>If you're interested, please <a href="#contact">contact me</a>.</p>
     <h2 id="sponsor-message">Sponsor Message</h2>
@@ -41,11 +40,11 @@
     <img class="max-width-100-percent border-2px-solid radius-8" src="/sponsor/mobile.png" alt="A screenshot of an example sponsor message appearing on a mobile device." title="An Example Sponsor Message Appearing on a Mobile Device">
 
     <h2 id="contact">Contact</h2>
-    <p>In order to get straight through to my high-priority mailbox, please send an email to:</p>
+    <p>If you'd like to enquire regarding a sponsorship, please send an email to:</p>
     <div class="centertext"><img class="max-width-100-percent" width="350px" src="/sponsor/sponsor-jamieweb.png"></div>
     <p class="margin-top-18">You can also contact me using any of the other methods available on my <a href="/contact/" target="_blank">contact page</a>.</p>
     <p>If you'd like to propose an alternate sponsorship or advertising idea, you're welcome to get in touch as well.</p>
-    <p>Thank you!</p>    
+    <p>Thank you!</p>
 </div>
 
 <?php include "footer.php" ?>
